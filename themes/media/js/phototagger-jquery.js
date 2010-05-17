694512e829b3f0f3d3bd0b5e3b8aebdc9d5b7997
ashish.anand <ashish.anand@rtcamp.com>
2010-05-17 17:24:16 +0000

    style modified

--- themes/media/js/phototagger-jquery.js
+++ themes/media/js/phototagger-jquery.js
@@ -57,8 +57,8 @@
 			){
 			
 			// Make this contianer relative.
-//			this.container.css( "position", "absolute" );
-                        //this.container.css( "height", "none !important" );
+			this.container.css( "position", "absolute" );
+                        this.container.css( "overflow", "hidden" );
                         //this.container.css( "width", "none !important" );
                         console.log('');
 			
