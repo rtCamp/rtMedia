6c51497ff6aeaf3f0a7fbedf4aa8768ff10d9bc9
kapil.gonge <kapil.gonge@rtcamp.com>
2010-05-18 17:53:37 +0000

    this version is in tags 1.1

--- themes/media/groups/single/media-upload.php
+++
@@ -1,7 +0,0 @@
-<div class="item-list-tabs no-ajax" id="subnav">
-	<ul>
-		<?php bp_media_group_media_tabs() ?>
-	</ul>
-</div>
-
-<?php bp_media_locate_template( array( 'media/upload.php' ), true );?>
