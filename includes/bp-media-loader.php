b4d3acd134ed265d8534b1f9f4d5e94ed6bea85e
Gagan Deep Singh <gagan.deep.singh@rtcamp.com>
2012-06-12 20:18:41 +0530

    URL Structuring complete(Not Tested Thoroughly)

--- includes/bp-media-loader.php
+++ includes/bp-media-loader.php
@@ -8,10 +8,11 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 
 //Constants
-define('BP_MEDIA_SLUG', 'media');
-define('BP_MEDIA_IMAGES_SLUG','images');
-define('BP_MEDIA_VIDEOS_SLUG','videos');
-define('BP_MEDIA_AUDIO_SLUG', 'audio');
+define( 'BP_MEDIA_SLUG', 'media' );
+define( 'BP_MEDIA_IMAGES_SLUG' , 'images' );
+define( 'BP_MEDIA_VIDEOS_SLUG' , 'videos' );
+define( 'BP_MEDIA_AUDIO_SLUG' , 'audio' );
+define( 'BP_MEDIA_UPLOAD_SLUG' , 'upload' );
 
 
 //To set the language according to the locale selected and availability of the language file.
@@ -57,18 +58,18 @@ class BP_Media_Component extends BP_Component {
 			'name' 		      => __( 'Media', 'bp-media' ),
 			'slug' 		      => BP_MEDIA_SLUG,
 			'position' 	      => 80,
-			'screen_function'     => 'bp_media_videos_screen',
-			'default_subnav_slug' => BP_MEDIA_VIDEOS_SLUG
+			'screen_function'     => 'bp_media_images_screen',
+			'default_subnav_slug' => BP_MEDIA_IMAGES_SLUG
 		);
 
 
 		
 		$sub_nav[] = array(
-			'name'            =>  __( 'Videos', 'bp-media' ),
-			'slug'            => BP_MEDIA_VIDEOS_SLUG,
+			'name'            =>  __( 'Upload', 'bp-media' ),
+			'slug'            => BP_MEDIA_UPLOAD_SLUG,
 			'parent_url'      => trailingslashit(bp_displayed_user_domain().BP_MEDIA_SLUG),
 			'parent_slug'     => BP_MEDIA_SLUG,
-			'screen_function' => 'bp_media_videos_screen',
+			'screen_function' => 'bp_media_upload_screen',
 			'position'        => 10
 		);
 
@@ -81,6 +82,16 @@ class BP_Media_Component extends BP_Component {
 			'name'				=>	__('Images','bp-media'),
 			'slug'				=>	BP_MEDIA_IMAGES_SLUG,
 			'screen_function'	=>	'bp_media_images_screen'			
+			));
+		bp_core_new_nav_item(array(
+			'name'				=>	__('Videos','bp-media'),
+			'slug'				=>	BP_MEDIA_VIDEOS_SLUG,
+			'screen_function'	=>	'bp_media_videos_screen'			
+			));
+		bp_core_new_nav_item(array(
+			'name'				=>	__('Audio','bp-media'),
+			'slug'				=>	BP_MEDIA_AUDIO_SLUG,
+			'screen_function'	=>	'bp_media_videos_screen'			
 			));
 //		bp_core_new_subnav_item( array(
 //			'name' 		  => __( 'Images', 'bp-media' ),
@@ -103,4 +114,28 @@ function bp_media_load_core_component() {
 }
 add_action( 'bp_loaded', 'bp_media_load_core_component' );
 
+function bp_media_custom_nav() {
+	global $bp;
+	foreach($bp->bp_nav as $key=>$nav_item) {
+		if($nav_item['slug']==BP_MEDIA_IMAGES_SLUG||$nav_item['slug']==BP_MEDIA_VIDEOS_SLUG||$nav_item['slug']==BP_MEDIA_AUDIO_SLUG) {
+			$bp->bp_options_nav[BP_MEDIA_SLUG][]=array(
+				'name'	=>	$nav_item['name'],
+				'link'	=>	$nav_item['link'],
+				'slug'	=>	$nav_item['slug'],
+				'css_id'	=>	$nav_item['css_id'],
+				'position'	=>	$nav_item['position'],
+				'screen_function'	=>	$nav_item['screen_function'],
+				'user_has_access'	=>	true,
+			);
+			unset($bp->bp_nav[$key]);
+		}
+	}
+	if($bp->current_component==BP_MEDIA_IMAGES_SLUG||$bp->current_component==BP_MEDIA_VIDEOS_SLUG||$bp->current_component==BP_MEDIA_AUDIO_SLUG){
+		$bp->current_action=$bp->current_component;
+		$bp->current_component=BP_MEDIA_SLUG;
+	}
+	
+	
+}
+add_action ( 'bp_setup_nav' , 'bp_media_custom_nav' , 999 );
 ?>
--- includes/bp-media-screens.php
+++ includes/bp-media-screens.php
@@ -32,9 +32,37 @@ function bp_media_videos_screen() {
 	function bp_media_videos_screen_content() {
 		global $bp;
 		_e('Test Content');
-		echo '<pre>';
-		var_dump($bp);
-		echo '</pre>';
 
+	}
+	
+function bp_media_audio_screen() {
+	
+	add_action( 'bp_template_title', 'bp_media_audio_screen_title' );
+	add_action( 'bp_template_content', 'bp_media_audio_screen_content' );
+
+	bp_core_load_template( apply_filters( 'bp_core_template_plugin', 'members/single/plugins' ) );
+}
+	function bp_media_audio_screen_title() {
+		_e('Audio');
+	}
+	function bp_media_audio_screen_content() {
+		global $bp;
+		_e('Test Content');
+
+	}
+	
+function bp_media_upload_screen() {
+	
+	add_action( 'bp_template_title', 'bp_media_upload_screen_title' );
+	add_action( 'bp_template_content', 'bp_media_upload_screen_content' );
+
+	bp_core_load_template( apply_filters( 'bp_core_template_plugin', 'members/single/plugins' ) );
+}
+	function bp_media_upload_screen_title() {
+		_e('Upload');
+	}
+	function bp_media_upload_screen_content() {
+		global $bp;
+		_e('Test Content');
 	}
 ?>
