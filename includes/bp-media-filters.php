46ae53b43523afd5e1b014800edb32fc16973c7f
Gagan Deep Singh <gagan.deep.singh@rtcamp.com>
2012-06-21 16:57:50 +0530

    Implemented the upload feature, checking for filetype complete, playing the videos with mediaelement player on activities generated by the upload of media

--- includes/bp-media-filters.php
+++ includes/bp-media-filters.php
@@ -10,14 +10,5 @@ function bp_media_activity_permalink_filter($link,$activity_obj) {
 }
 add_filter('bp_activity_get_permalink', 'bp_media_activity_permalink_filter',10,2);
 
-function bp_media_upload_type_change_filter() {
-	global $bp;
-//	if(isset($bp->{BP_MEDIA_SLUG})) {
-//		echo '<pre>';
-//		var_dump(func_get_args());
-//		echo '</pre>';
-//	}
-}
-//add_filter('wp_handle_upload','bp_media_upload_type_change_filter',10,3);
 
 ?>
--- includes/bp-media-template-functions.php
+++ includes/bp-media-template-functions.php
@@ -7,12 +7,12 @@
 function bp_media_show_upload_form() {
 	global $bp;
 	?>
-	<form method="post" enctype="multipart/form-data">
-		<input type="text" name="bp_media_title" />
-		<input type="text" name="bp_media_description" />
-		<input type="file" name="bp_media_file" />
-		<input type="hidden" name="action" value="wp_handle_upload"/>
-		<input type="submit" />
+	<form method="post" enctype="multipart/form-data" class="standard-form" id="bp-media-upload-form">
+		<label for="bp-media-upload-input-title"><?php _e('Media Title','bp-media'); ?></label><input id="bp-media-upload-input-title" type="text" name="bp_media_title" class="settings-input" />
+		<label for="bp-media-upload-input-description"><?php _e('Media Description','bp-media'); ?></label><input id="bp-media-upload-input-description" type="text" name="bp_media_description" class="settings-input" />
+		<label for="bp-media-upload-file"><?php _e('Select Media File','bp-media') ?></label><input type="file" name="bp_media_file" id="bp-media-upload-file" />
+		<input type="hidden" name="action" value="wp_handle_upload" />
+		<div class="submit"><input type="submit" class="auto" value="Upload" /></div>
 	</form>
 	<?php
 }
