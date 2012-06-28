4f497b0e57ca5cd1b6a86dfe19f4fd3ddb78bbad
Gagan Deep Singh <gagan.deep.singh@rtcamp.com>
2012-06-28 14:52:27 +0530

    Implementing shortcodes, commit for backup

--- includes/bp-media-class-wordpress.php
+++ includes/bp-media-class-wordpress.php
@@ -3,28 +3,47 @@
 /**
  * 
  */
-class BP_Media_Host_Wordpress implements BP_Media {
+class BP_Media_Host_Wordpress implements BP_Media_Host {
 
 	private $id, //id of the entry
 		$name, //Name of the entry
 		$description, //Description of the entry
 		$url, //URL of the entry
 		$type, //Type of the entry (Video, Image or Audio)
-		$owner;   //Owner of the entry.
+		$owner;   //Owner of the entry
 
-	function __construct($media_id = '') {
+	function __construct($media_id = 0) {
 		if (!$media_id == '') {
 			$this->init($media_id);
 		}
 	}
 
 	function init($media_id = '') {
-		if (!$media_id == '') {
-			$media = get_post($media_id);
-			echo '<pre>';
-			var_dump($media);
-			echo '</pre>';
+		if (is_object($media_id)) {
+			$media = $media_id;
+		} else {
+			$media = &get_post($media_id);
 		}
+		if (empty($media->ID))
+			return false;
+		$this->id= $media->ID;
+		$this->description=$media->post_content;
+		$this->name= $media->post_title;
+		$this->owner=$media->post_author;
+		$this->type=  get_post_meta($media->ID,'bp_media_type',true);
+		switch ($this->type) {
+				case 'video' : 
+					$this->url=trailingslashit(bp_loggedin_user_domain() . BP_MEDIA_IMAGES_SLUG . '/watch/' . $$this->id);
+					break;
+				case 'audio' : 
+					$this->url=trailingslashit(bp_loggedin_user_domain() . BP_MEDIA_IMAGES_SLUG . '/listen/' . $this->id);
+					break;
+				case 'image' :
+					$this->url = trailingslashit(bp_loggedin_user_domain() . BP_MEDIA_IMAGES_SLUG . '/view/' . $this->id);
+					break;
+				default : 
+					return false;
+			}
 	}
 
 	function add_media($name, $description) {
@@ -60,20 +79,16 @@ class BP_Media_Host_Wordpress implements BP_Media {
 			'post_content' => $content,
 			'post_parent' => $post_id,
 		);
-		$activity_content = '<span class="bp_media_title">' . $name . '</span><span class="bp_media_description">' . $description . '</span><span class="bp_media_content">';
 		switch ($type) {
-			case 'video/mp4' : $activity_content.='<video src="' . $url . '" width="640" height="480" type="video/mp4" id="bp_media_video_' . $post_id . '" controls="controls" preload="none"></video><script>jQuery("#bp_media_video_' . $post_id . '").mediaelementplayer();</script></span>';
-				$activity_url = trailingslashit(bp_loggedin_user_domain() . BP_MEDIA_VIDEOS_SLUG . '/watch/' . $post_id);
+			case 'video/mp4' :
 				$type = 'video';
 				break;
-			case 'audio/mpeg' : $activity_content.='<audio src="' . $url . '" type="audio/mp3" id="bp_media_audio_' . $post_id . '" controls="controls" preload="none" ></audio><script>jQuery("#bp_media_audio_' . $post_id . '").mediaelementplayer();</script>';
-				$activity_url = trailingslashit(bp_loggedin_user_domain() . BP_MEDIA_AUDIO_SLUG . '/listen/' . $post_id);
+			case 'audio/mpeg' :
 				$type = 'audio';
 				break;
 			case 'image/gif' :
 			case 'image/jpeg' :
-			case 'image/png' : $activity_content.='<img src="' . $url . '" id="bp_media_image_' . $post_id . '" />';
-				$activity_url = trailingslashit(bp_loggedin_user_domain() . BP_MEDIA_IMAGES_SLUG . '/view/' . $post_id);
+			case 'image/png' :
 				$type = 'image';
 				break;
 			default : unlink($file);
@@ -82,7 +97,7 @@ class BP_Media_Host_Wordpress implements BP_Media {
 				$activity_content = false;
 				return false;
 		}
-		$activity_content .= '</span>';
+		$activity_content = '[bp_media_content id="' . $post_id . '"]';
 		$attachment_id = wp_insert_attachment($attachment, $file, $post_id);
 		if (!is_wp_error($attachment_id)) {
 			wp_update_attachment_metadata($attachment_id, wp_generate_attachment_metadata($attachment_id, $file));
@@ -97,9 +112,9 @@ class BP_Media_Host_Wordpress implements BP_Media {
 		//$wpdb->update( $wpdb->posts, $postarr, array( 'ID' => $post_id ) );
 		wp_insert_post($postarr);
 		$activity_id = bp_media_record_activity(array(
-			'action' => sprintf(__("%s uploaded a media."), bp_core_get_userlink(bp_loggedin_user_id())),
+			'action' => '[bp_media_action id="' . $post_id . '"]',
 			'content' => $activity_content,
-			'primary_link' => $activity_url,
+			'primary_link' => '[bp_media_url id="' . $post_id . '"]',
 			'type' => 'media_upload'
 			));
 		bp_activity_update_meta($activity_id, 'bp_media_parent_post', $post_id);
@@ -125,6 +140,47 @@ class BP_Media_Host_Wordpress implements BP_Media {
 
 	function display_media() {
 		
+	}
+
+	function display_media_activity_content() {
+		if (bp_is_activity_component()) {
+			$activity_content = '<span class="bp_media_title">' . $this->name . '</span><span class="bp_media_description">' . $description . '</span><span class="bp_media_content">';
+			switch ($type) {
+				case 'video/mp4' : $activity_content.='<video src="' . $url . '" width="640" height="480" type="video/mp4" id="bp_media_video_' . $post_id . '" controls="controls" preload="none"></video><script>jQuery("#bp_media_video_' . $post_id . '").mediaelementplayer();</script></span>';
+					$type = 'video';
+					break;
+				case 'audio/mpeg' : $activity_content.='<audio src="' . $url . '" type="audio/mp3" id="bp_media_audio_' . $post_id . '" controls="controls" preload="none" ></audio><script>jQuery("#bp_media_audio_' . $post_id . '").mediaelementplayer();</script>';
+					$type = 'audio';
+					break;
+				case 'image/gif' :
+				case 'image/jpeg' :
+				case 'image/png' : $activity_content.='<img src="' . $url . '" id="bp_media_image_' . $post_id . '" />';
+					$activity_url = trailingslashit(bp_loggedin_user_domain() . BP_MEDIA_IMAGES_SLUG . '/view/' . $post_id);
+					$type = 'image';
+					break;
+				default : unlink($file);
+					wp_delete_post($post_id, true);
+					unlink($file);
+					$activity_content = false;
+					return false;
+			}
+			$activity_content .= '</span>';
+			return $activity_content;
+		}
+	}
+
+	function display_media_activity_url() {
+		if (!bp_is_activity_component())
+			return false;
+		$activity_url = bp_media_get_the_permalink($this->id);
+		return $activity_url;
+	}
+
+	function display_media_activity_action() {
+		if (!bp_is_activity_component())
+			return false;
+		$activity_action=sprintf(__("%s uploaded a media."), bp_core_get_userlink($this->owner));
+		return $activity_action;
 	}
 
 }
--- includes/bp-media-filters.php
+++ includes/bp-media-filters.php
@@ -4,10 +4,22 @@
  * 
  */
 function bp_media_activity_permalink_filter($link, $activity_obj) {
-	if ('media_upload' == $activity_obj->type)
-		$link = $activity_obj->primary_link;
+	if ('media_upload' == $activity_obj->type) {
+		add_shortcode('bp_media_url', 'bp_media_shortcode_url');
+		$link = do_shortcode($activity_obj->primary_link);
+		remove_shortcode('bp_media_url');
+	}
 	return $link;
 }
 
 add_filter('bp_activity_get_permalink', 'bp_media_activity_permalink_filter', 10, 2);
+
+function test($activity_action) {
+	add_shortcode('bp_media_action', 'bp_media_shortcode_action');
+	$activity_action = do_shortcode($activity_action);
+	remove_shortcode('bp_media_action');
+	return $activity_action;
+}
+
+add_filter('bp_get_activity_action', 'test');
 ?>
