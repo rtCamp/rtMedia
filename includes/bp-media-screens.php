92005963e856bdf2065f1389e13d57a8fd427c2c
Gagan Deep Singh <gagan.deep.singh@rtcamp.com>
2012-08-29 13:17:47 +0530

    Edit feature complete

--- includes/bp-media-screens.php
+++ includes/bp-media-screens.php
@@ -36,12 +36,28 @@ function bp_media_images_screen() {
 	if (isset($bp->action_variables[0])) {
 		switch ($bp->action_variables[0]) {
 			case BP_MEDIA_IMAGES_EDIT_SLUG :
+				global $bp_media_current_entry;
+				
+				
+				//Creating global bp_media_current_entry for later use
+				try {
+					$bp_media_current_entry = new BP_Media_Host_Wordpress($bp->action_variables[1]);
+					
+				} catch (Exception $e) {
+					/* Send the values to the cookie for page reload display */
+					@setcookie('bp-message', $_COOKIE['bp-message'], time() + 60 * 60 * 24, COOKIEPATH);
+					@setcookie('bp-message-type', $_COOKIE['bp-message-type'], time() + 60 * 60 * 24, COOKIEPATH);
+					wp_redirect(trailingslashit(bp_displayed_user_domain() . BP_MEDIA_IMAGES_SLUG));
+					exit;
+				}
+				//For saving the data if the form is submitted
+				if(array_key_exists('bp_media_title', $_POST)){
+					$bp_media_current_entry->update_media(array('name'=> esc_html($_POST['bp_media_title']),'description'=> esc_html($_POST['bp_media_description'])));
+				}
 				bp_media_images_edit_screen();
 				break;
 			case BP_MEDIA_IMAGES_ENTRY_SLUG:
 				global $bp_media_current_entry;
-				if (!$bp->action_variables[0] == BP_MEDIA_IMAGES_ENTRY_SLUG)
-					return false;
 				try {
 					$bp_media_current_entry = new BP_Media_Host_Wordpress($bp->action_variables[1]);
 				} catch (Exception $e) {
@@ -52,6 +68,9 @@ function bp_media_images_screen() {
 					exit;
 				}
 				add_action('bp_template_content', 'bp_media_images_entry_screen_content');
+				break;
+			case BP_MEDIA_DELETE_SLUG :
+				bp_media_entry_delete();
 				break;
 			default:
 				bp_media_set_query();
@@ -103,12 +122,22 @@ function bp_media_images_edit_screen() {
 }
 
 function bp_media_images_edit_screen_title() {
-	_e('Images Edit Page');
+	_e('Edit Image','bp-media');
 }
 
 function bp_media_images_edit_screen_content() {
-	global $bp;
-	_e('Images Edit Page Content');
+	global $bp, $bp_media_current_entry,$bp_media_default_excerpts;
+	?>
+	<form method="post" class="standard-form" id="bp-media-upload-form">
+		<label for="bp-media-upload-input-title"><?php _e('Media Title', 'bp-media'); ?></label><input id="bp-media-upload-input-title" type="text" name="bp_media_title" class="settings-input" maxlength="<?php echo max(array($bp_media_default_excerpts['single_entry_title'],$bp_media_default_excerpts['activity_entry_title'])) ?>" value="<?php echo $bp_media_current_entry->get_title(); ?>" />
+		<label for="bp-media-upload-input-description"><?php _e('Media Description', 'bp-media'); ?></label><input id="bp-media-upload-input-description" type="text" name="bp_media_description" class="settings-input" maxlength="<?php echo max(array($bp_media_default_excerpts['single_entry_description'],$bp_media_default_excerpts['activity_entry_description'])) ?>" value="<?php echo $bp_media_current_entry->get_content(); ?>" />
+		<div class="submit"><input type="submit" class="auto" value="Update" /></div>
+	</form>
+	<?php
+//	echo '<div class="bp-media-single bp-media-image">';
+//	echo $bp_media_current_entry->get_media_single_content();
+//	//echo $bp_media_current_entry->show_comment_form();
+//	echo '</div>';
 }
 
 /**
@@ -334,5 +363,9 @@ function bp_media_audio_entry_screen_content() {
 	echo $bp_media_current_entry->show_comment_form();
 	echo '</div>';
 	do_action('bp_media_after_content');
+}
+
+function bp_media_entry_delete() {
+	global $bp;
 }
 ?>
