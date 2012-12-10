8969019f3b1f5e1252af2e93e31ca9747bb12a22
gagan0123 <gagan@iamme.in>
2012-12-10 20:55:07 +0530

    Updated functions for pagination for groups, updated BP Media class for generating urls for group media

--- includes/js/bp-media.js
+++ includes/js/bp-media.js
@@ -50,7 +50,8 @@ jQuery(document).ready(function(){
 			current_action : bp_media_vars.current_action,
 			action_variables : bp_media_vars.action_variables,
 			displayed_user : bp_media_vars.displayed_user,
-			loggedin_user : bp_media_vars.loggedin_user
+			loggedin_user : bp_media_vars.loggedin_user,
+			current_group :	bp_media_vars.current_group
 		};
 
 		// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
@@ -72,11 +73,11 @@ jQuery(document).ready(function(){
 		});
 
 	},1000);
-	
+
 	        /* Add Featured Image */
-        
+
         jQuery('.bp-media-featured').live('click',function(e){
-        e.preventDefault();        
+        e.preventDefault();
         var post_id = jQuery(this).attr('data-post-id');
         var post_date = new Date();
         var date = post_date.getFullYear()+'-'+(post_date.getMonth() + 1) +'-'+post_date.getDate()+' '+ post_date.getHours()+':'+(post_date.getMinutes() + 1)+':'+(post_date.getSeconds()+1);
@@ -85,7 +86,7 @@ jQuery(document).ready(function(){
         var remove_featured = 0;
         if(jQuery(this).attr('data-remove-featured')){
             remove_featured = jQuery(this).attr('data-remove-featured');
-        }        
+        }
         jQuery.ajax({
             url:"/wp-admin/admin-ajax.php",
             type:'POST',
@@ -99,7 +100,7 @@ jQuery(document).ready(function(){
                     curr_obj.text('Remove Featured');
                     curr_obj.attr('data-remove-featured','1');
                 }
-                
+
             }
         });
     });
