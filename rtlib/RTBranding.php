38eab4108f4f106c0593ac39129472e64ed30b84
root <joshua.bangera@gmail.com>
2012-12-17 13:14:02 +0530

    Created Helper Classes

---
+++ rtlib/RTBranding.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Description of RTBranding
+ *
+ * @author Gagandeep Singh <gagandeep.singh@rtcamp.com>
+ * @author Joshua Abenazer <joshua.abenazer@rtcamp.com>
+ */
+if (!class_exists('RTBranding')) {
+
+    class RTBranding {
+
+        public function __construct() {
+            ;
+        }
+
+    }
+
+}
+?>
