4205ca55ff6f020790917edbc011d47c98489d0a
yapapaya <saurabh.shukla@rtcamp.com>
2012-12-19 16:56:43 +0530

    more refactors

---
+++ app/main/BPMediaLoader.php
@@ -0,0 +1,24 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) )
+	exit;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of BPMediaLoader
+ *
+ * @author saurabh
+ */
+class BPMediaLoader {
+
+	public function __construct(){
+
+	}
+
+
+}
+
+?>
