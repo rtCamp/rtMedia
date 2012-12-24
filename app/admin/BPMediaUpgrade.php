ad2141675b816c0e710fcb2803210b208a97f13a
root <joshua.bangera@gmail.com>
2012-12-24 12:33:46 +0530

    Refactoring addons

--- app/admin/BPMediaUpgrade.php
+++ app/admin/BPMediaUpgrade.php
@@ -5,7 +5,7 @@
  * @author Gagandeep Singh <gagandeep.singh@rtcamp.com>
  * @author Joshua Abenazer <joshua.abenazer@rtcamp.com>
  */
-if (!class_exists(BPMediaUpgrade)) {
+if (!class_exists('BPMediaUpgrade')) {
 
     class BPMediaUpgrade {
 
