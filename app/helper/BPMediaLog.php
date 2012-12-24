5e20a19858c4403fec18e90adc5bf81ea0ffd943
root <joshua.bangera@gmail.com>
2012-12-24 15:20:14 +0530

    Refactoring Addns

---
+++ app/helper/BPMediaLog.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of BPMediaLog
+ *
+ * @author Joshua Abenazer <joshua.abenazer@rtcamp.com>
+ */
+if (!class_exists('BPMediaLog')) {
+
+    class BPMediaLog {
+
+        public function __construct() {
+            ;
+        }
+
+    }
+
+}
+?>
