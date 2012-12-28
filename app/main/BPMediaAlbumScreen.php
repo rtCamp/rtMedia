fb0a66357910c7d92d88fe1a27488ce0d8fefc98
yapapaya <saurabh.shukla@rtcamp.com>
2012-12-28 12:15:41 +0530

    Albums and Media refactored. Upload's broken. Activities still broken

--- app/main/BPMediaAlbumScreen.php
+++ app/main/BPMediaAlbumScreen.php
@@ -16,6 +16,87 @@ class BPMediaAlbumScreen extends BPMediaScreen {
 		parent::__construct( $media_type, $slug );
 	}
 
+	function screen() {
+		global $bp;
+		if ( isset( $bp->action_variables[ 0 ] ) ) {
+			print_r( $bp->action_variables );
+			switch ( $bp->action_variables[ 0 ] ) {
+				case BP_MEDIA_ALBUMS_EDIT_SLUG :
+					$this->edit_screen();
+					break;
+				case BP_MEDIA_ALBUMS_ENTRY_SLUG:
+					$this->entry_screen();
+					$this->template_actions( 'entry_screen' );
+					break;
+				case BP_MEDIA_DELETE_SLUG :
+					if ( ! isset( $bp->action_variables[ 1 ] ) ) {
+						$this->page_not_exist();
+					}
+					$this->entry_delete();
+					break;
+				default:
+					$this->set_query();
+					$this->template_actions( 'screen' );
+			}
+		} else {
+			$this->set_query();
+			$this->template_actions( 'screen' );
+		}
+		$this->template_loader();
+	}
+
+	function screen_content() {
+		global $bp_media, $bp_media_albums_query;
+
+		$this->hook_before();
+		if ( $bp_media_albums_query && $bp_media_albums_query->have_posts() ):
+			echo '<ul id="bp-media-list" class="bp-media-gallery item-list">';
+			while ( $bp_media_albums_query->have_posts() ) : $bp_media_albums_query->the_post();
+				$this->album_content();
+			endwhile;
+			echo '</ul>';
+			$this->show_more();
+		else:
+			bp_media_show_formatted_error_message( sprintf( __( 'Sorry, no %s were found.', $bp_media->text_domain ), $this->slug ), 'info' );
+		endif;
+		$this->hook_after();
+	}
+
+	function entry_screen() {
+		global $bp, $bp_media_current_album;
+		if ( ! $bp->action_variables[ 0 ] == BP_MEDIA_ALBUMS_ENTRY_SLUG )
+			return false;
+		try {
+			$bp_media_current_album = new BPMediaAlbum( $bp->action_variables[ 1 ] );
+		} catch ( Exception $e ) {
+			/* Send the values to the cookie for page reload display */
+			@setcookie( 'bp-message', $_COOKIE[ 'bp-message' ], time() + 60 * 60 * 24, COOKIEPATH );
+			@setcookie( 'bp-message-type', $_COOKIE[ 'bp-message-type' ], time() + 60 * 60 * 24, COOKIEPATH );
+			$this->template_redirect();
+			exit;
+		}
+	}
+
+	function entry_screen_content() {
+		global $bp, $bp_media_current_album, $bp_media_query;
+		if ( ! $bp->action_variables[ 0 ] == BP_MEDIA_ALBUMS_ENTRY_SLUG )
+			return false;
+		echo '<div class="bp_media_title">' . $bp_media_current_album->get_title() . '</div>';
+		$this->inner_query( $bp_media_current_album->get_id() );
+		if ( $bp_media_current_album && $bp_media_query->have_posts() ):
+			do_action( 'bp_media_before_content' );
+			echo '<ul id="bp-media-list" class="bp-media-gallery item-list">';
+			while ( $bp_media_query->have_posts() ) : $bp_media_query->the_post();
+				$this->the_content();
+			endwhile;
+			echo '</ul>';
+			$this->show_more();
+			do_action( 'bp_media_after_content' );
+		else:
+			bp_media_show_formatted_error_message( __( 'Sorry, no media items were found in this album.', 'bp-media' ), 'info' );
+		endif;
+	}
+
 	function set_query() {
 		global $bp, $bp_media_albums_query;
 		if ( isset( $bp->action_variables ) && is_array( $bp->action_variables ) && isset( $bp->action_variables[ 0 ] ) && $bp->action_variables[ 0 ] == 'page' && isset( $bp->action_variables[ 1 ] ) && is_numeric( $bp->action_variables[ 1 ] ) ) {
@@ -36,7 +117,7 @@ class BPMediaAlbumScreen extends BPMediaScreen {
 		}
 	}
 
-	function the_content( $id = 0 ) {
+	function album_content( $id = 0 ) {
 		if ( is_object( $id ) )
 			$album = $id;
 		else
@@ -46,11 +127,36 @@ class BPMediaAlbumScreen extends BPMediaScreen {
 		if ( ! $album->post_type == 'bp_media_album' )
 			return false;
 		try {
-			$album = new BP_Media_Album( $album->ID );
+			$album = new BPMediaAlbum( $album->ID );
 			echo $album->get_album_gallery_content();
 		} catch ( Exception $e ) {
 			echo '';
 		}
+	}
+
+	function template_actions( $action ) {
+		add_action( 'bp_template_content', array( $this, $action . '_content' ) );
+	}
+
+	function inner_query( $album_id = 0 ) {
+		global $bp, $bp_media_query;
+		$paged = 0;
+		$action_variables = isset( $bp->canonical_stack[ 'action_variables' ] ) ? $bp->canonical_stack[ 'action_variables' ] : null;
+		if ( isset( $action_variables ) && is_array( $action_variables ) && isset( $action_variables[ 0 ] ) ) {
+			if ( $action_variables[ 0 ] == 'page' && isset( $action_variables[ 1 ] ) && is_numeric( $action_variables[ 1 ] ) )
+				$paged = $action_variables[ 1 ];
+			else if ( isset( $action_variables[ 1 ] ) && $action_variables[ 1 ] == 'page' && isset( $action_variables[ 2 ] ) && is_numeric( $action_variables[ 2 ] ) )
+				$paged = $action_variables[ 2 ];
+		}
+		if ( ! $paged )
+			$paged = 1;
+		$args = array(
+			'post_type' => 'attachment',
+			'post_status' => 'any',
+			'post_parent' => $album_id,
+			'paged' => $paged
+		);
+		$bp_media_query = new WP_Query( $args );
 	}
 
 }
