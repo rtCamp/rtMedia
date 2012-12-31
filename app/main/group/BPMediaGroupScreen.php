b5729b58b73bb45fe563cd23c7fdb87170bd36ec
yapapaya <saurabh.shukla@rtcamp.com>
2012-12-31 15:43:52 +0530

    More refactoring, widgets refactored

---
+++ app/main/group/BPMediaGroupScreen.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of BPMediaGroupScreen
+ *
+ * @author saurabh
+ */
+class BPMediaGroupScreen {
+	//put your code here
+}
+
+?>
