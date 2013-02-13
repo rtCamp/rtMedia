75e9fd3bb94636f27b418a032cf59f2d63d54e68
Joshua Abenazer <joshua.abenazer@rtcamp.com>
2013-02-13 15:51:58 +0530

    Group Activity and Normal Activity Media Upload

--- app/main/BPMediaGroupLoader.php
+++ app/main/BPMediaGroupLoader.php
@@ -2,8 +2,8 @@
 /**
  * Don't load this file directly!
  */
-if ( ! defined( 'ABSPATH' ) )
-	exit;
+if (!defined('ABSPATH'))
+    exit;
 
 /**
  * Loads Group Media functionality
@@ -13,32 +13,31 @@ if ( ! defined( 'ABSPATH' ) )
  */
 class BPMediaGroupLoader {
 
-	/**
-	 * Constructs all the group functionality
-	 * Loads dummy classes
-	 * Adds necessary navigation and tabs
-	 *
-	 */
+    /**
+     * Constructs all the group functionality
+     * Loads dummy classes
+     * Adds necessary navigation and tabs
+     *
+     */
     function __construct() {
-		global $bp_media;
-		$enabled = $bp_media->enabled();
+        global $bp_media;
+        $enabled = $bp_media->enabled();
 
 
-            if (class_exists('BPMediaGroupsExtension')) :
-                bp_register_group_extension('BPMediaGroupsExtension');
-			if($enabled['images'])
+        if (class_exists('BPMediaGroupsExtension')) :
+            bp_register_group_extension('BPMediaGroupsExtension');
+            if ($enabled['images'])
                 new BPMediaGroupImages ();
-                new BPMediaGroupAlbums();
-			if($enabled['audio'])
+            new BPMediaGroupAlbums();
+            if ($enabled['audio'])
                 new BPMediaGroupAudio();
-			if($enabled['videos'])
+            if ($enabled['videos'])
                 new BPMediaGroupVideos();
-                new BPMediaGroupUpload();
-            endif;
-            add_action('bp_actions', array($this, 'custom_nav'), 999);
-            add_filter('bp_media_multipart_params_filter',
-					array($this, 'multipart_params_handler')
-					);
+            new BPMediaGroupUpload();
+        endif;
+        add_action('bp_actions', array($this, 'custom_nav'), 999);
+        add_filter('bp_media_multipart_params_filter', array($this, 'multipart_params_handler')
+        );
     }
 
     /**
@@ -57,17 +56,15 @@ class BPMediaGroupLoader {
     function custom_nav() {
         global $bp;
         $current_group = isset($bp->groups->current_group->slug) ?
-		$bp->groups->current_group->slug
-				: null;
+                $bp->groups->current_group->slug : null;
         if (!$current_group)
             return;
         if (!(isset($bp->bp_options_nav[$current_group])
-				&& is_array($bp->bp_options_nav[$current_group])))
+                && is_array($bp->bp_options_nav[$current_group])))
             return;
 
         /** This line might break a thing or two in custom themes and widgets */
-        remove_filter('bp_activity_get_user_join_filter',
-				'BPMediaFilters::activity_query_filter', 10);
+        remove_filter('bp_activity_get_user_join_filter', 'BPMediaFilters::activity_query_filter', 10);
 
         foreach ($bp->bp_options_nav[$current_group] as $key => $nav_item) {
             switch ($nav_item['slug']) {
@@ -112,10 +109,10 @@ class BPMediaGroupLoader {
     function multipart_params_handler($multipart_params) {
         if (is_array($multipart_params)) {
             global $bp;
-            if (isset($bp->current_action) && $bp->current_action == BP_MEDIA_SLUG
+            if (isset($bp->current_action) && ( ($bp->current_action == BP_MEDIA_SLUG) || bp_is_group_home() )
                     && isset($bp->action_variables)
-					&& isset($bp->current_component)
-					&& $bp->current_component == 'groups'
+                    && isset($bp->current_component)
+                    && $bp->current_component == 'groups'
                     && isset($bp->groups->current_group->id)) {
                 $multipart_params['bp_media_group_id'] = $bp->groups->current_group->id;
             }
@@ -138,8 +135,8 @@ class BPMediaGroupLoader {
      * @return boolean
      */
     static function navigation_menu() {
-        global $bp,$bp_media;
-		$enabled = $bp_media->enabled();
+        global $bp, $bp_media;
+        $enabled = $bp_media->enabled();
 
         if (!isset($bp->current_action) || $bp->current_action != BP_MEDIA_SLUG)
             return false;
@@ -173,12 +170,12 @@ class BPMediaGroupLoader {
                     );
                 }
             } else {
-				if($enabled[strtolower($type)]){
-                $bp_media_nav[constant('BP_MEDIA_' . $type . '_SLUG')] = array(
-                    'url' => trailingslashit(bp_get_group_permalink($bp->groups->current_group)) . constant('BP_MEDIA_' . $type . '_SLUG'),
-                    'label' => constant('BP_MEDIA_' . $type . '_LABEL'),
-                );
-				}
+                if ($enabled[strtolower($type)]) {
+                    $bp_media_nav[constant('BP_MEDIA_' . $type . '_SLUG')] = array(
+                        'url' => trailingslashit(bp_get_group_permalink($bp->groups->current_group)) . constant('BP_MEDIA_' . $type . '_SLUG'),
+                        'label' => constant('BP_MEDIA_' . $type . '_LABEL'),
+                    );
+                }
             }
         }
 
@@ -187,115 +184,117 @@ class BPMediaGroupLoader {
         ?>
         <div class="item-list-tabs no-ajax bp-media-group-navigation" id="subnav">
             <ul>
-                <?php
-                foreach ($bp_media_group_tabs as $tab_slug => $tab_info) {
-                    echo '<li id="' . $tab_slug . '-group-li" ' . ($current_tab == $tab_slug ? 'class="current selected"' : '') . '><a id="' . $tab_slug . '" href="' . $tab_info['url'] . '" title="' . __($tab_info['label'], BP_MEDIA_TXT_DOMAIN) . '">' . __($tab_info['label'], BP_MEDIA_TXT_DOMAIN) . '</a></li>';
-                }
-                ?>
+        <?php
+        foreach ($bp_media_group_tabs as $tab_slug => $tab_info) {
+            echo '<li id="' . $tab_slug . '-group-li" ' . ($current_tab == $tab_slug ? 'class="current selected"' : '') . '><a id="' . $tab_slug . '" href="' . $tab_info['url'] . '" title="' . __($tab_info['label'], BP_MEDIA_TXT_DOMAIN) . '">' . __($tab_info['label'], BP_MEDIA_TXT_DOMAIN) . '</a></li>';
+        }
+        ?>
             </ul>
         </div>
-        <?php
-    }
+                <?php
+            }
 
-    /**
-     * Checks whether the current logged in user has the ability to upload on
-     * the given group or not
-     *
-     * @since BuddyPress Media 2.3
-     */
+            /**
+             * Checks whether the current logged in user has the ability to upload on
+             * the given group or not
+             *
+             * @since BuddyPress Media 2.3
+             */
 
-    /**
-     *
-     * @global type $bp
-     * @return boolean
-     */
-    static function can_upload() {
-        /** @todo Implementation Pending */
-        global $bp;
-        if (isset($bp->loggedin_user->id) && is_numeric($bp->loggedin_user->id) && class_exists( 'BP_Group_Extension' )) {
-            return groups_is_user_member($bp->loggedin_user->id, bp_get_current_group_id());
-        } else {
-            return false;
-        }
+            /**
+             *
+             * @global type $bp
+             * @return boolean
+             */
+            static function can_upload() {
+                /** @todo Implementation Pending */
+                global $bp;
+                if (isset($bp->loggedin_user->id) && is_numeric($bp->loggedin_user->id) && class_exists('BP_Group_Extension')) {
+                    return groups_is_user_member($bp->loggedin_user->id, bp_get_current_group_id());
+                } else {
+                    return false;
+                }
 
-        return true;
-    }
+                return true;
+            }
 
-    /**
-     * Adds the Media Settings menu for groups in the admin bar
-     *
-     * @uses global $bp,$wp_admin_bar
-     *
-     * @since BuddyPress Media 2.3
-     */
+            /**
+             * Adds the Media Settings menu for groups in the admin bar
+             *
+             * @uses global $bp,$wp_admin_bar
+             *
+             * @since BuddyPress Media 2.3
+             */
 
-    /**
-     *
-     * @global type $wp_admin_bar
-     * @global type $bp
-     */
-    function admin_bar() {
-        global $wp_admin_bar, $bp;
-        $wp_admin_bar->add_menu(array(
-            'parent' => $bp->group_admin_menu_id,
-            'id' => 'bp-media-group',
-            'title' => __('Media Settings', BP_MEDIA_TXT_DOMAIN),
-            'href' => bp_get_groups_action_link('admin/media')
-        ));
-    }
+            /**
+             *
+             * @global type $wp_admin_bar
+             * @global type $bp
+             */
+            function admin_bar() {
+                global $wp_admin_bar, $bp;
+                $wp_admin_bar->add_menu(array(
+                    'parent' => $bp->group_admin_menu_id,
+                    'id' => 'bp-media-group',
+                    'title' => __('Media Settings', BP_MEDIA_TXT_DOMAIN),
+                    'href' => bp_get_groups_action_link('admin/media')
+                ));
+            }
 
-    //add_action('admin_bar_menu','admin_bar',99);
-    /* This will need some handling for checking if its a single group page or not, also whether the person can
-     * edit media settings or not
-     */
+            //add_action('admin_bar_menu','admin_bar',99);
+            /* This will need some handling for checking if its a single group page or not, also whether the person can
+             * edit media settings or not
+             */
 
-    /**
-     * Checks whether a user can create an album in the given group or not
-     *
-     * @param string $group_id The group id to check against
-     * @param string $user_id The user to be checked for permission
-     *
-     * @return boolean True if the user can create an album in the group, false if not
-     */
+            /**
+             * Checks whether a user can create an album in the given group or not
+             *
+             * @param string $group_id The group id to check against
+             * @param string $user_id The user to be checked for permission
+             *
+             * @return boolean True if the user can create an album in the group, false if not
+             */
 
-    /**
-     *
-     * @param type $group_id
-     * @param type $user_id
-     * @return boolean
-     */
-    static function user_can_create_album($group_id, $user_id = 0) {
-        if ($user_id == 0)
-            $user_id = get_current_user_id();
-        $current_level = groups_get_groupmeta($group_id, 'bp_media_group_control_level');
-        switch ($current_level) {
-            case 'all':
-                return groups_is_user_member($user_id, $group_id) || groups_is_user_mod($user_id, $group_id) || groups_is_user_admin($user_id, $group_id);
-                break;
-            case 'moderators':
-                return groups_is_user_mod($user_id, $group_id) || groups_is_user_admin($user_id, $group_id);
-                break;
-            case 'admin':
-                return groups_is_user_admin($user_id, $group_id);
-                break;
-            default :
-                return groups_is_user_admin($user_id, $group_id);
-        }
-        return false;
-    }
+            /**
+             *
+             * @param type $group_id
+             * @param type $user_id
+             * @return boolean
+             */
+            static function user_can_create_album($group_id, $user_id = 0) {
+                if ($user_id == 0)
+                    $user_id = get_current_user_id();
+                $current_level = groups_get_groupmeta($group_id, 'bp_media_group_control_level');
+                switch ($current_level) {
+                    case 'all':
+                        return groups_is_user_member($user_id, $group_id) || groups_is_user_mod($user_id, $group_id) || groups_is_user_admin($user_id, $group_id);
+                        break;
+                    case 'moderators':
+                        return groups_is_user_mod($user_id, $group_id) || groups_is_user_admin($user_id, $group_id);
+                        break;
+                    case 'admin':
+                        return groups_is_user_admin($user_id, $group_id);
+                        break;
+                    default :
+                        return groups_is_user_admin($user_id, $group_id);
+                }
+                return false;
+            }
 
-    /**
-     *
-     * @param type $errorMessage
-     */
-    static function bp_media_display_error($errorMessage) {
-        ?>
+            /**
+             *
+             * @param type $errorMessage
+             */
+            static function bp_media_display_error($errorMessage) {
+                ?>
         <div id="message" class="error">
             <p>
-                <?php _e($errorMessage, BP_MEDIA_TXT_DOMAIN); ?>
+        <?php _e($errorMessage, BP_MEDIA_TXT_DOMAIN); ?>
             </p>
         </div>
-        <?php
-    }
+                <?php
+            }
 
-}
+        }
+
+        
