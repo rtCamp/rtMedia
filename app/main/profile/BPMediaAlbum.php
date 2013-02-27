2bdfa76f378fc8264fb6b5c4105d400ed59848f8
yapapaya <saurabh.shukla@rtcamp.com>
2013-02-27 11:52:09 +0530

    Fixed album privacy save

--- app/main/profile/BPMediaAlbum.php
+++ app/main/profile/BPMediaAlbum.php
@@ -128,6 +128,7 @@ class BPMediaAlbum {
             $this->thumbnail = '<img src ="' . BP_MEDIA_URL . 'app/assets/img/image_thumb.png">';
         }
 		$this->filter_entries();
+		$this->type = 'album';
         $this->media_entries = get_children(array(
             'post_parent' => $this->id,
             'post_type' => 'attachment',
