77d74a73d898579bfe8701d7854f6b911e7827e2
Joshua Abenazer <joshua.abenazer@rtcamp.com>
2013-05-23 19:56:22 +0530

    BPMedia Upload Exception class, File Upload and Insert Attachment Handled

--- app/main/upload/processors/BPMediaUploadFile.php
+++ app/main/upload/processors/BPMediaUploadFile.php
@@ -3,49 +3,165 @@
 /**
  * Description of BPMediaUploadFile
  *
- * @author joshua
+ * @author Joshua Abenazer <joshua.abenazer@rtcamp.com>
  */
 class BPMediaUploadFile {
-    
-    private $default_allowed_types = 'audio|video|image';
-    
-    public function __construct($uploaded) {
-        
-        $_POST['action'] = 'wp_handle_upload'; //to pass the form submission test
-        
-        if ( !isset($_FILES['bp_media_file']) || !is_array($_FILES['bp_media_file']) )
-            return;
-        
-        if ($type = $this->is_allowed_type($_FILES['bp_media_file'])){
+
+    private $default_allowed_types = array('audio', 'video', 'image');
+    var $files;
+    var $fake = false;
+
+    function init($files) {
+
+        $this->allowed_types = apply_filters('bp_media_allowed_types', $this->default_allowed_types);
+        $this->set_file($files);
+        $this->unset_invalid_files();
+        include_once(ABSPATH . 'wp-admin/includes/file.php');
+        include_once(ABSPATH . 'wp-admin/includes/image.php');
+
+        $uploaded_file = $this->process();
+        return $uploaded_file;
+    }
+
+    function process() {
+        $function = $this->fake ? 'wp_handle_sideload' : 'wp_handle_upload';
+        foreach ($this->files as $key => $file) {
             
-        } else {
-            return;
+            $uploaded_file[] = $function($file, array('test_form' => false));
+            
+            try {
+                if (isset($uploaded_file[$key]['error']) || $uploaded_file[$key] === null) {
+                    array_pop($uploaded_file);
+
+                    throw new Exception(__('Error Uploading File', 'buddypress-media'));
+                }
+                $uploaded_file[$key]['name'] = $file['name'];
+            } catch (Exception $e) {
+                echo $e->message();
+            }
+            
+            if (strpos($file['type'],'image') !== false ) {
+                if (function_exists('read_exif_data')) {
+                    $file = $this->exif($uploaded_file);
+                }
+            }
+            
         }
-        
-        $class_name = apply_filters('bp_media_transcoder', 'BPMediaHostWordpress', $type);
-        $bp_media_entry = new $class_name();
-        try {
-            $title = pathinfo($_FILES['bp_media_file']['name'], PATHINFO_FILENAME);
-            $album_id = 204;
-            $is_multiple = false;
-            $is_activity = false;
-            $description = 'asd';
-            $group_id = 0;
-            $entry = $bp_media_entry->add_media($title, $description, $album_id, $group_id, $is_multiple, $is_activity);
-            if (!isset($bp->{BP_MEDIA_SLUG}->messages['updated'][0]))
-                $bp->{BP_MEDIA_SLUG}->messages['updated'][0] = __('Upload Successful', 'buddypress-media');
-        } catch (Exception $e) {
-            echo $e->getMessage();
+
+        return $uploaded_file;
+    }
+
+    function set_file($files) {
+        if ($files) {
+            $this->fake = true;
+            $this->populate_file_array((array) $uploaded['files']);
+        } elseif (isset($_FILES['bp_media_file'])) {
+            $this->populate_file_array(
+                    $this->arrayify($_FILES['bp_media_file'])
+            );
+        } else {
+            throw new BPMediaUploadException(UPLOAD_ERR_NO_FILE);
         }
     }
-    
-    function is_allowed_type($file_array){
-        $allowed_types = apply_filters('bp_media_allowed_types',$this->default_allowed_types);
-        if (!preg_match('/'.$allowed_types.'/i', $file_array['type'], $result) || !isset($result[0])) {
-//            $bp->{BP_MEDIA_SLUG}->messages['error'][] = __('File uploaded is not supported');
+
+    function populate_file_array($file_array) {
+        foreach ($file_array as $file) {
+            $this->files[] = array(
+                'name' => isset($file['name']) ? $file['name'] : '',
+                'type' => isset($file['type']) ? $file['type'] : '',
+                'tmp_name' => isset($file['tmp_name']) ? $file['tmp_name'] : '',
+                'error' => isset($file['error']) ? $file['error'] : '',
+                'size' => isset($file['size']) ? $file['size'] : 0,
+            );
+        }
+    }
+
+    function is_valid_type($file) {
+        try {
+            if (!preg_match('/' . implode('|', $this->allowed_types) . '/i', $file['type'], $result) || !isset($result[0])) {
+                throw new BPMediaUploadException(UPLOAD_ERR_EXTENSION);
+            }
+        } catch (BPMediaUploadException $e) {
             return false;
         }
-        return $result[0];
+        $this->type = $result[0];
+        return true;
+    }
+
+    function unset_invalid_files() {
+        foreach ($this->files as $key => $file) {
+            if (!$this->is_valid_type($file)) {
+                unset($this->files[$key]);
+            }
+        }
+    }
+
+    function exif($file) {
+
+        $file_parts = pathinfo($file['file']);
+        if (in_array(strtolower($file_parts['extension']), array('jpg', 'jpeg', 'tiff'))) {
+            $exif = read_exif_data($file['file']);
+            $exif_orient = isset($exif['Orientation']) ? $exif['Orientation'] : 0;
+            $rotateImage = 0;
+
+            if (6 == $exif_orient) {
+                $rotateImage = 90;
+                $imageOrientation = 1;
+            } elseif (3 == $exif_orient) {
+                $rotateImage = 180;
+                $imageOrientation = 1;
+            } elseif (8 == $exif_orient) {
+                $rotateImage = 270;
+                $imageOrientation = 1;
+            }
+
+            if ($rotateImage) {
+                if (class_exists('Imagick')) {
+                    $imagick = new Imagick();
+                    $imagick->readImage($file['file']);
+                    $imagick->rotateImage(new ImagickPixel(), $rotateImage);
+                    $imagick->setImageOrientation($imageOrientation);
+                    $imagick->writeImage($file['file']);
+                    $imagick->clear();
+                    $imagick->destroy();
+                } else {
+                    $rotateImage = -$rotateImage;
+
+                    switch ($file['type']) {
+                        case 'image/jpeg':
+                            $source = imagecreatefromjpeg($file['file']);
+                            $rotate = imagerotate($source, $rotateImage, 0);
+                            imagejpeg($rotate, $file['file']);
+                            break;
+                        case 'image/png':
+                            $source = imagecreatefrompng($file['file']);
+                            $rotate = imagerotate($source, $rotateImage, 0);
+                            imagepng($rotate, $file['file']);
+                            break;
+                        case 'image/gif':
+                            $source = imagecreatefromgif($file['file']);
+                            $rotate = imagerotate($source, $rotateImage, 0);
+                            imagegif($rotate, $file['file']);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+        return $file;
+    }
+    
+    function arrayify($files) {
+        if (isset($files['name']) && !is_array($files['name'])) {
+            $updated_files[0] = $files;
+        } else {
+            foreach($files as $key => $array) {
+                foreach($array as $index => $value)
+                    $updated_files[$index][$key] = $value;
+            }
+        }
+        return $updated_files;
     }
 }
 
--- app/main/upload/processors/BPMediaUploadPost.php
+++ app/main/upload/processors/BPMediaUploadPost.php
@@ -11,7 +11,128 @@
  * @author joshua
  */
 class BPMediaUploadPost {
-    //put your code here
+
+    var $post_array = false;
+
+    function init($uploaded, $file_object) {
+        $sanitized_file_object = $this->sanitize_type($file_object);
+//        print_r($sanitized_file_object);
+        $attachments = $this->generate_post_array($sanitized_file_object);
+//        print_r($attachments);die;
+        return $this->insert_attachment($attachments,$sanitized_file_object);
+    }
+
+    function generate_post_array($file_object) {
+//        error_log(var_export($file_object,true));die;
+        foreach ($file_object as $file) {
+            $attachments[] = array(
+                'post_mime_type' => $file['type'],
+                'guid' => $file['url'],
+                'post_title' => $uploaded['title'] ? $uploaded['title'] : $file['name'],
+                'post_content' => $uploaded['description'] ? $uploaded['description'] : '',
+                'post_parent' => $uploaded['album_id'] ? $uploaded['album_id'] : 0,
+            );
+        }
+        return $attachments;
+    }
+
+    function sanitize_type($file_object) {
+        foreach ($file_object as $key => $file) {
+            switch ($file['type']) {
+                case 'video/mp4' :
+                case 'video/quicktime' :
+                    $type = 'video';
+                    include_once(trailingslashit(BP_MEDIA_PATH) . 'lib/getid3/getid3.php');
+                    try {
+                        $getID3 = new getID3;
+                        $vid_info = $getID3->analyze($file['file']);
+                    } catch (Exception $e) {
+                        unlink($file['file']);
+                        $activity_content = false;
+                        throw new Exception(__('MP4 file you have uploaded is corrupt.', 'buddypress-media'));
+                    }
+                    if (is_array($vid_info)) {
+                        if (!array_key_exists('error', $vid_info) && array_key_exists('fileformat', $vid_info) && array_key_exists('video', $vid_info) && array_key_exists('fourcc', $vid_info['video'])) {
+                            if (!($vid_info['fileformat'] == 'mp4' && $vid_info['video']['fourcc'] == 'avc1')) {
+                                unlink($file['file']);
+                                $activity_content = false;
+                                throw new Exception(__('The MP4 file you have uploaded is using an unsupported video codec. Supported video codec is H.264.', 'buddypress-media'));
+                            }
+                        } else {
+                            unlink($file['file']);
+                            $activity_content = false;
+                            throw new Exception(__('The MP4 file you have uploaded is using an unsupported video codec. Supported video codec is H.264.', 'buddypress-media'));
+                        }
+                    } else {
+                        unlink($file['file']);
+                        $activity_content = false;
+                        throw new Exception(__('The MP4 file you have uploaded is not a video file.', 'buddypress-media'));
+                    }
+                    break;
+                case 'audio/mpeg' :
+                    include_once(trailingslashit(BP_MEDIA_PATH) . 'lib/getid3/getid3.php');
+                    try {
+                        $getID3 = new getID3;
+                        $file_info = $getID3->analyze($file['file']);
+                    } catch (Exception $e) {
+                        unlink($file['file']);
+                        $activity_content = false;
+                        throw new Exception(__('MP3 file you have uploaded is currupt.', 'buddypress-media'));
+                    }
+                    if (is_array($file_info)) {
+                        if (!array_key_exists('error', $file_info) && array_key_exists('fileformat', $file_info) && array_key_exists('audio', $file_info) && array_key_exists('dataformat', $file_info['audio'])) {
+                            if (!($file_info['fileformat'] == 'mp3' && $file_info['audio']['dataformat'] == 'mp3')) {
+                                unlink($file['file']);
+                                $activity_content = false;
+                                throw new Exception(__('The MP3 file you have uploaded is using an unsupported audio format. Supported audio format is MP3.', 'buddypress-media'));
+                            }
+                        } else {
+                            unlink($file['file']);
+                            $activity_content = false;
+                            throw new Exception(__('The MP3 file you have uploaded is using an unsupported audio format. Supported audio format is MP3.', 'buddypress-media'));
+                        }
+                    } else {
+                        unlink($file['file']);
+                        $activity_content = false;
+                        throw new Exception(__('The MP3 file you have uploaded is not an audio file.', 'buddypress-media'));
+                    }
+                    $type = 'audio';
+                    break;
+                case 'image/gif' :
+                case 'image/jpeg' :
+                case 'image/png' :
+                    $type = 'image';
+                    break;
+                default :
+                    unlink($file['file']);
+                    $activity_content = false;
+                    throw new Exception(__('Media File you have tried to upload is not supported. Supported media files are .jpg, .png, .gif, .mp3, .mov and .mp4.', 'buddypress-media'));
+            }
+            $sanitized_file_object[] = $file;
+        }
+        
+        return $sanitized_file_object;
+    }
+    
+    function insert_attachment($attachments,$file_object){
+        foreach( $attachments as $key => $attachment) {
+            $attachment_id = wp_insert_attachment($attachment, $file_object[$key]['file'], $attachment['post_parent']);
+            if (!is_wp_error($attachment_id)) {
+                add_filter('intermediate_image_sizes', array($this, 'bp_media_image_sizes'), 99);
+                wp_update_attachment_metadata($attachment_id, wp_generate_attachment_metadata($attachment_id, $file_object[$key]['file']));
+            } else {
+                unlink($file_object[$key]['file']);
+                throw new Exception(__('Error creating attachment for the media file, please try again', 'buddypress-media'));
+            }
+            $updated_attachment_ids[] = $attachment;
+        }
+        return $updated_attachment_ids;
+    }
+    
+    function bp_media_image_sizes($sizes) {
+        return array('bp_media_thumbnail', 'bp_media_activity_image', 'bp_media_single_image');
+    }
+
 }
 
 ?>
