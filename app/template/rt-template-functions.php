823f6d5428a494531ca76ab1d92c229cc881a575
yapapaya <saurabh.shukla@rtcamp.com>
2013-05-24 19:10:33 +0530

    added initial templates, template system and started writing queries

---
+++ app/template/rt-template-functions.php
@@ -0,0 +1,39 @@
+<?php
+
+function has_rt_media(){
+
+}
+
+function rt_media(){
+
+}
+
+function rt_media_title(){
+
+}
+
+function rt_media_permalink(){
+
+}
+
+function rt_media_thumbnail(){
+
+}
+
+function rt_media_content(){
+
+}
+
+function rt_media_description(){
+
+}
+
+function rt_media_actions(){
+
+}
+
+function rt_media_comments(){
+
+}
+
+?>
