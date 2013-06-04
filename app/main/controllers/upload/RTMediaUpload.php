4f2e079b9c9b2370886aed5f95354d636a9e3d18
Udit Desai <desaiuditd@gmail.com>
2013-06-04 21:29:01 +0530

    media and album controllers for uploads

--- app/main/controllers/upload/RTMediaUpload.php
+++ app/main/controllers/upload/RTMediaUpload.php
@@ -9,21 +9,19 @@ class RTMediaUpload {
 
     private $default_modes = array('file_upload', 'link_input');
     var $file = NULL;
-    var $post = NULL;
+    var $media = NULL;
     var $url = NULL;
-    var $db = NULL;
 
     public function __construct($uploaded) {
         $this->file = new RTMediaUploadFile();
         $this->url = new RTMediaUploadUrl();
-        $this->post = new RTMediaUploadPost();
-        $this->db = new RTMediaUploadDB();
+        $this->media = new RTMediaMedia();
 
         $file_object = $this->upload($uploaded);
 
         if ($file_object) {
-            if($this->post->init($uploaded, $file_object)){
-                do_action('bp_media_after_add_media');
+            if($this->media->add($uploaded, $file_object)){
+                do_action('rt_media_after_add_media');
             }
         } else {
             return false;
