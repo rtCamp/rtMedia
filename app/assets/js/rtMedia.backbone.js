ae237300a580cb2782ceae81995cd1abe05bd20a
Faishal <saiyedfaishal@gmail.com>
2013-06-21 19:53:02 +0530

    fix backbone paging

--- app/assets/js/rtMedia.backbone.js
+++ app/assets/js/rtMedia.backbone.js
@@ -1,3 +1,6 @@
+var galleryObj;
+var nextpage = 2;
+
 jQuery(function($) {
 
 
@@ -6,6 +9,12 @@ jQuery(function($) {
 		rtMedia = window.rtMedia || {};
 
 	rtMedia.Context = Backbone.Model.extend({
+                url: function(){
+                    var url = "media/";
+                    if(nextpage>0)
+                        url += 'page/' + nextpage + '/'
+                    return url;
+                },
 		defaults: {
 			"context"		: "post",
 			"context_id"	: false
@@ -31,27 +40,32 @@ jQuery(function($) {
 			"likes"				: 0,
 			"dislikes"			: 0,
 			"guid"				: false,
-			"has_next"			: false,
-			"has_prev"			: false
+			"next"			: -1,
+			"prev"			: -1
 		}
 
 	});
 
 	rtMedia.Gallery = Backbone.Collection.extend({
 		model: rtMedia.Media,
-		url: 'media/',
+		url: function(){
+                    var url = 'media/';
+                    if(nextpage >1)
+                        url += 'page/' + nextpage + '/';
+                    return url;
+                },
 
 		getNext: function(page) {
 			this.fetch({
 				data: {
 					json: true,
-					rt_media_page: page
+					rt_media_page: nextpage
 				},
 				success: function(model, response) {
-					//var galleryViewObj = new rtMedia.GalleryView({
-				//		collection: model,
-			//			el: $(".rt-media-list")[0]
-		//			});
+					var galleryViewObj = new rtMedia.GalleryView({
+					collection: model,
+                        		el: $(".rt-media-list")[0] });
+                                        
 				}
 			});
 		},
@@ -88,11 +102,16 @@ jQuery(function($) {
 			this.render();
 		},
 		render: function(){
-			$(this.el).html("");
+			//$(this.el).html("");
 			that = this;
 			$.each(this.collection.toJSON(), function(key, media){
 				$(that.el).append(that.template(media));
+                                   nextpage = media.next;
 			});
+                        if(nextpage > 1){
+                            $("#rtMedia-galary-next").show();
+                        }
+                        
 			
 		},
 		appendTo: function(media) {
@@ -105,6 +124,12 @@ jQuery(function($) {
 	});
 
 
-	var galleryObj = new rtMedia.Gallery();
+	galleryObj = new rtMedia.Gallery();
+        $(document).on("click","#rtMedia-galary-next",function(e){
+            $(this).hide();
+            e.preventDefault();
+            
+            galleryObj.getNext(nextpage);
+        });
 
 });
