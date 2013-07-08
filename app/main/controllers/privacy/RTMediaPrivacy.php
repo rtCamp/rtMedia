1b4046ba574899991a83c98c0d0128e4f0d050a2
Faishal <saiyedfaishal@gmail.com>
2013-07-08 17:43:41 +0530

    fix query

--- app/main/controllers/privacy/RTMediaPrivacy.php
+++ app/main/controllers/privacy/RTMediaPrivacy.php
@@ -238,7 +238,8 @@ class RTMediaPrivacy {
         else
             $bp_prefix = "";
 
-
+                $select_sql = str_replace("SELECT", "SELECT distinct", $select_sql);
+                
 		$from_sql = " FROM {$bp->activity->table_name} a LEFT JOIN {$wpdb->users} u ON a.user_id = u.ID LEFT JOIN {$bp->activity->table_name_meta} m ON a.id = m.activity_id";
 		$where_sql = $where_sql . " AND (NOT EXISTS (SELECT m.activity_id FROM {$bp_prefix}bp_activity_meta m WHERE m.meta_key='rtmedia_privacy' AND m.activity_id=a.id) OR (m.meta_key='rtmedia_privacy' {$where} ) )";
 		$newsql = "{$select_sql} {$from_sql} {$where_sql} ORDER BY a.date_recorded {$sort} {$pag_sql}";
