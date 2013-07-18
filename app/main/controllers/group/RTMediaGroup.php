e460779ff6c9f04c7b37b98fc251d970997ce434
Faishal <saiyedfaishal@gmail.com>
2013-07-18 20:54:22 +0530

    Handle bp_is_active on activation

--- app/main/controllers/group/RTMediaGroup.php
+++ app/main/controllers/group/RTMediaGroup.php
@@ -7,7 +7,7 @@
 class RTMediaGroup{
     public $create_slug = "media-setting";
     function __construct() {
-        if(bp_is_active("groups")){
+        if(function_exists("bp_is_active") &&  bp_is_active("groups")){
             bp_register_group_extension("RTMediaGroupExtension");
         }
         
