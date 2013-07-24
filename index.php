83fde531875b550f4c3b56f6cbb6882acccc81a2
Faishal <saiyedfaishal@gmail.com>
2013-07-24 17:01:46 +0530

    format code

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
   Plugin Name: rtMedia for WordPress, BuddyPress and bbPress
   Plugin URI: http://rtcamp.com/buddypress-media/?utm_source=dashboard&utm_medium=plugin&utm_campaign=buddypress-media
@@ -15,44 +16,42 @@
  * @package BuddyPressMedia
  * @subpackage Main
  */
+if ( ! defined ( 'RTMEDIA_PATH' ) ) {
 
-if ( ! defined( 'RTMEDIA_PATH' ) ){
-
-	/**
-	 *  The server file system path to the plugin directory
-	 *
-	 */
-	define( 'RTMEDIA_PATH', plugin_dir_path( __FILE__ ) );
+    /**
+     *  The server file system path to the plugin directory
+     *
+     */
+    define ( 'RTMEDIA_PATH', plugin_dir_path ( __FILE__ ) );
 }
 
-if ( ! defined( 'BP_MEDIA_PATH' ) ){
+if ( ! defined ( 'BP_MEDIA_PATH' ) ) {
 
-	/**
-	 *  Legacy support
-	 *
-	 */
-	define( 'BP_MEDIA_PATH', RTMEDIA_PATH );
+    /**
+     *  Legacy support
+     *
+     */
+    define ( 'BP_MEDIA_PATH', RTMEDIA_PATH );
 }
 
 
-if ( ! defined( 'RTMEDIA_URL' ) ){
+if ( ! defined ( 'RTMEDIA_URL' ) ) {
 
-	/**
-	 * The url to the plugin directory
-	 *
-	 */
-	define( 'RTMEDIA_URL', plugin_dir_url( __FILE__ ) );
+    /**
+     * The url to the plugin directory
+     *
+     */
+    define ( 'RTMEDIA_URL', plugin_dir_url ( __FILE__ ) );
 }
 
-if ( ! defined( 'RTMEDIA_BASE_NAME' ) ){
+if ( ! defined ( 'RTMEDIA_BASE_NAME' ) ) {
 
-	/**
-	 * The url to the plugin directory
-	 *
-	 */
-	define( 'RTMEDIA_BASE_NAME', plugin_basename( __FILE__ ) );
+    /**
+     * The url to the plugin directory
+     *
+     */
+    define ( 'RTMEDIA_BASE_NAME', plugin_basename ( __FILE__ ) );
 }
-
 
 /**
  * Auto Loader Function
@@ -61,47 +60,47 @@ if ( ! defined( 'RTMEDIA_BASE_NAME' ) ){
  *
  * @param string $class_name The name of the class to autoload
  */
-function rtmedia_autoloader( $class_name ) {
-	$rtlibpath = array(
-		'app/services/' . $class_name . '.php',
-		'app/helper/' . $class_name . '.php',
+function rtmedia_autoloader ( $class_name ) {
+    $rtlibpath = array(
+        'app/services/' . $class_name . '.php',
+        'app/helper/' . $class_name . '.php',
         'app/helper/db/' . $class_name . '.php',
-		'app/admin/' . $class_name . '.php',
-		'app/main/interactions/' . $class_name . '.php',
-		'app/main/routers/' . $class_name . '.php',
-		'app/main/routers/query/' . $class_name . '.php',
-		'app/main/controllers/upload/' . $class_name . '.php',
-		'app/main/controllers/upload/processors/' . $class_name . '.php',
-		'app/main/controllers/shortcodes/' . $class_name . '.php',
-		'app/main/controllers/template/' . $class_name . '.php',
-		'app/main/controllers/media/' . $class_name . '.php',
-		'app/main/controllers/group/' . $class_name . '.php',
-		'app/main/controllers/privacy/' . $class_name . '.php',
-		'app/main/controllers/activity/' . $class_name . '.php',
-		'app/main/deprecated/' . $class_name . '.php',
-		'app/main/contexts/' . $class_name . '.php',
-		'app/main/' . $class_name . '.php',
-		'app/main/includes/' . $class_name . '.php',
-		'app/main/widgets/' . $class_name . '.php',
-		'app/main/upload/' . $class_name . '.php',
-		'app/main/upload/processors/' . $class_name . '.php',
-		'app/main/template/' . $class_name . '.php',
-		'app/log/' . $class_name . '.php',
-		'app/importers/' . $class_name . '.php',
-	);
-	foreach ( $rtlibpath as $path ) {
-		$path = RTMEDIA_PATH . $path;
-		if ( file_exists( $path ) ) {
-			include $path;
-			break;
-		}
-	}
+        'app/admin/' . $class_name . '.php',
+        'app/main/interactions/' . $class_name . '.php',
+        'app/main/routers/' . $class_name . '.php',
+        'app/main/routers/query/' . $class_name . '.php',
+        'app/main/controllers/upload/' . $class_name . '.php',
+        'app/main/controllers/upload/processors/' . $class_name . '.php',
+        'app/main/controllers/shortcodes/' . $class_name . '.php',
+        'app/main/controllers/template/' . $class_name . '.php',
+        'app/main/controllers/media/' . $class_name . '.php',
+        'app/main/controllers/group/' . $class_name . '.php',
+        'app/main/controllers/privacy/' . $class_name . '.php',
+        'app/main/controllers/activity/' . $class_name . '.php',
+        'app/main/deprecated/' . $class_name . '.php',
+        'app/main/contexts/' . $class_name . '.php',
+        'app/main/' . $class_name . '.php',
+        'app/main/includes/' . $class_name . '.php',
+        'app/main/widgets/' . $class_name . '.php',
+        'app/main/upload/' . $class_name . '.php',
+        'app/main/upload/processors/' . $class_name . '.php',
+        'app/main/template/' . $class_name . '.php',
+        'app/log/' . $class_name . '.php',
+        'app/importers/' . $class_name . '.php',
+    );
+    foreach ( $rtlibpath as $path ) {
+        $path = RTMEDIA_PATH . $path;
+        if ( file_exists ( $path ) ) {
+            include $path;
+            break;
+        }
+    }
 }
 
 /**
  * Register the autoloader function into spl_autoload
  */
-spl_autoload_register( 'rtmedia_autoloader' );
+spl_autoload_register ( 'rtmedia_autoloader' );
 
 /**
  * Instantiate the BuddyPressMedia class.
@@ -112,4 +111,3 @@ $rtmedia = new RTMedia();
 /*
  * Look Ma! Very few includes! Next File: /app/main/RTMedia.php
  */
-?>
