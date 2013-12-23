26178c520b681c8c66e8c3dc225bccb30792f7d4
Pushpak <pushpak.pop@gmail.com>
2013-12-23 13:59:01 +0530

    changed conditional check for options

--- app/main/controllers/activity/RTMediaBuddyPressActivity.php
+++ app/main/controllers/activity/RTMediaBuddyPressActivity.php
@@ -14,7 +14,7 @@ class RTMediaBuddyPressActivity {
 
     function __construct () {
         global $rtmedia;
-        if ( $rtmedia->options[ "buddypress_enableOnActivity" ] !== 0 ) {
+        if ( $rtmedia->options[ "buddypress_enableOnActivity" ] != 0 ) {
             add_action ( "bp_after_activity_post_form", array( &$this, "bp_after_activity_post_form" ) );
             add_action ( "bp_activity_posted_update", array( &$this, "bp_activity_posted_update" ), 99, 3 );
             add_action ( "bp_groups_posted_update", array( &$this, "bp_groups_posted_update" ), 99, 4 );
