ec8c5dc47bf3bfa95586223a8100a188d951dc60
ritesh.patel <ritesh.patel@rtcamp.com>
2014-01-09 15:24:14 +0530

    admin notice for template override

--- app/admin/RTMediaAdmin.php
+++ app/admin/RTMediaAdmin.php
@@ -1363,13 +1363,11 @@ if ( ! class_exists ( 'RTMediaAdmin' ) ) {
         }
 
         function rtmedia_update_template_notice(){
-	    $site_option  = rtmedia_get_site_option("rtmedia-update-template-notice");
+	    $site_option  = rtmedia_get_site_option("rtmedia-update-template-notice-v3_4_7");
             if(!$site_option || $site_option != "hide") {
-		rtmedia_update_site_option("rtmedia-update-template-notice", "show");
-		echo '<div class="error rtmedia-update-template-notice"><p>'
-                . __('Template files of rtMedia Plugin are updated, so please update your rtMedia template files if you have overridden the default rtMedia templates in your theme.')
-                . '<a href="#" onclick="rtmedia_hide_template_override_notice()" style="float:right">' .__('Hide', 'rtmedia') .'</a>'
-                . ' </p></div>';
+		rtmedia_update_site_option("rtmedia-update-template-notice-v3_4_7", "show");
+		if( is_dir(get_template_directory().'/rtmedia') ) {
+		    echo '<div class="error rtmedia-update-template-notice"><p>' . __('rtMedia just updated to Foundation 5. Please update rtMedia template files if you have overridden the default rtMedia templates in your theme.') . '<a href="#" onclick="rtmedia_hide_template_override_notice()" style="float:right">' .__('Hide', 'rtmedia') .'</a>' . ' </p></div>';
 	    ?>
 		<script type="text/javascript">
 		    function rtmedia_hide_template_override_notice() {
@@ -1382,12 +1380,13 @@ if ( ! class_exists ( 'RTMediaAdmin' ) ) {
 		    }
 		</script>
 	    <?php
+		}
 	    }
 	}
 
          function rtmedia_hide_template_override_notice() {
 
-	    if(rtmedia_update_site_option("rtmedia-update-template-notice", "hide"))
+	    if(rtmedia_update_site_option("rtmedia-update-template-notice-v3_4_7", "hide"))
 		echo "1";
 	    else
 		echo "0";
