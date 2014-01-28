0b61e7782d7820b71683d050e852c29aac3192e9
Faishal Saiyed <saiyedfaishal@gmail.com>
2014-01-28 15:49:25 +0530

    Update travis.yml

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,7 +15,7 @@ $GLOBALS['wp_tests_options'] = array(
 require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
 
 function _manually_load_plugin() {
-        require dirname( __FILE__ ) . '/../wp-seo.php';
+        require dirname( __FILE__ ) . '/../index.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
