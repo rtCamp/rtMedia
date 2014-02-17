2fcf53f5f3b26cb898d549a13a450a23bde55ef1
ritesh.patel <ritesh.patel@rtcamp.com>
2014-02-17 15:23:11 +0530

    fix js issue, fix global albumcover art in group, added query filter

--- app/assets/js/rtMedia.backbone.js
+++ app/assets/js/rtMedia.backbone.js
@@ -280,7 +280,7 @@ jQuery(function($) {
 
         uploaderObj.uploader.bind('UploadComplete', function(up, files) {
 	    activity_id = -1;
-            if( rtmedia_gallery_reload_on_upload =='1'){ //reload gallery view when upload completes if enabled( by default enabled)
+            if( typeof rtmedia_gallery_reload_on_upload != "undefined" && rtmedia_gallery_reload_on_upload =='1'){ //reload gallery view when upload completes if enabled( by default enabled)
                 galleryObj.reloadView();
             }
             jQuery('.start-media-upload').hide();
