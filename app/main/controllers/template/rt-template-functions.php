67b4e162413209fe3e3977fa75f4199d0dce7692
ritesh.patel <ritesh.patel@rtcamp.com>
2014-02-18 16:10:11 +0530

    fix media author delete group media issue

--- app/main/controllers/template/rt-template-functions.php
+++ app/main/controllers/template/rt-template-functions.php
@@ -407,7 +407,7 @@ function rtmedia_delete_allowed () {
 
     $flag = $rtmedia_media->media_author == get_current_user_id ();
 
-    if( isset($rtmedia_media->context) && $rtmedia_media->context == 'group' && function_exists('bp_group_is_admin')){
+    if( !$flag && isset($rtmedia_media->context) && $rtmedia_media->context == 'group' && function_exists('bp_group_is_admin')){
             $flag = ( bp_group_is_admin() || bp_group_is_mod());
     }
 
