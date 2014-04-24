325ba2e7051a83b80840ae38ce37e6dbc016244b
ritesh.patel <ritesh.patel@rtcamp.com>
2014-04-24 15:10:04 +0530

    enable lightbox in mobile devices

--- app/assets/js/rtMedia.js
+++ app/assets/js/rtMedia.js
@@ -15,7 +15,7 @@ function apply_rtMagnificPopup(selector){
                 tLoading: rt_load_more + ' #%curr%...',
                 mainClass: 'mfp-img-mobile',
                 preload: [1, 3],
-                closeOnBgClick: false,
+                closeOnBgClick: true,
                 gallery: {
                     enabled: true,
                     navigateByImgClick: true,
@@ -28,12 +28,12 @@ function apply_rtMagnificPopup(selector){
                         return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
                     }
                 },
-                disableOn: function() {
-                    if (jQuery(window).width() < 600) {
-                        return false;
-                    }
-                    return true;
-                },
+//                disableOn: function() {
+//                    if (jQuery(window).width() < 600) {
+//                        return false;
+//                    }
+//                    return true;
+//                },
                 callbacks: {
                     ajaxContentAdded: function() {
 
