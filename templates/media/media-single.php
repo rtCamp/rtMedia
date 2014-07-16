6c5d03c7c204adbdc297922a9c0e6e93a1c1b8b5
Sanket Parmar <sanket.parmar11@gmail.com>
2014-07-16 13:15:18 +0530

    Updated minor code change in single media page for css-class

--- templates/media/media-single.php
+++ templates/media/media-single.php
@@ -21,11 +21,7 @@
                     <!--author actions-->
                     <div class='rtm-ltb-title-container rt-clear'>
                         <h2 class='rtm-ltb-title'>
-                            <?php if(rtmedia_album_name()) { ?>
-                                <div class="rtmedia-media-name rtmedia-media-name-width-50">
-                            <?php } else { ?>
-                                <div class="rtmedia-media-name rtmedia-media-name-width-100">
-                            <?php } ?>
+                            <div class="rtmedia-media-name <?php if(rtmedia_album_name()) { ?>rtmedia-media-name-width-50<?php } else { ?>rtmedia-media-name-width-100<?php } ?>">
                                 <a href="<?php echo rtmedia_permalink();?>" title="<?php echo rtmedia_title (); ?>"><?php echo rtmedia_title (); ?></a>
                             </div>
                             <?php if(rtmedia_album_name()) { ?>
