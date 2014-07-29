1d33d0981eda628f663ef893c1b446676812be9c
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2014-07-29 17:24:03 +0530

    removed trailing space

--- app/helper/BPMediaBranding.php
+++ app/helper/BPMediaBranding.php
@@ -5,11 +5,11 @@
  *
  * @package BuddyPressMedia
  * @subpackage Admin
- * 
+ *
  * @author Gagandeep Singh <gagandeep.singh@rtcamp.com>
  * @author Joshua Abenazer <joshua.abenazer@rtcamp.com>
  */
-if (!class_exists('BPMediaBranding')) {
+if ( ! class_exists( 'BPMediaBranding' ) ){
 
     class BPMediaBranding {
 
