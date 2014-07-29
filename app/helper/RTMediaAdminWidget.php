98a56e15a7d2e9d5ad736deaf19b3e7c9095e7a2
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2014-07-29 14:39:27 +0530

    removed trailing space

--- app/helper/RTMediaAdminWidget.php
+++ app/helper/RTMediaAdminWidget.php
@@ -22,8 +22,8 @@ if ( ! class_exists( 'RTMediaAdminWidget' ) ){
 				?>
 				<div class="postbox" id="<?php echo $id; ?>">
 				<?php if ( $title ){ ?>
-						<h3 class="hndle"><span><?php echo $title; ?></span></h3>
-		        <?php }
+							<h3 class="hndle"><span><?php echo $title; ?></span></h3>
+				<?php }
 				?>
 				    <div class="inside"><?php echo $content; ?></div>
 				</div><?php
