7ea9b7360ca8e021a35f92873a5869be5c2bba24
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2014-07-29 12:25:05 +0530

    added function header to functions

--- app/helper/RTMediaInteractionModel.php
+++ app/helper/RTMediaInteractionModel.php
@@ -22,30 +22,57 @@ class RTMediaInteractionModel extends RTDBModel {
 	    parent::__construct ( 'rtm_media_interaction', false, 10, true );
 	}
 
-    function check($user_id = "", $media_id = "", $action = "") {
-        if($user_id == "" || $media_id == "" || $action == "")
-            return false;
-        $columns = array(
-            'user_id' => $user_id,
-            'media_id' => $media_id,
-            'action' => $action
-        );
-        $results = $this->get($columns);
-        if($results)
-            return true;
-        else
-            return false;
-    }
+	/**
+	 * Check user id and media id.
+	 *
+	 * @access public
+	 * @param  int  $user_id
+	 * @param  int  $media_id
+	 * @param  type $action
+	 * @return bool
+	 */
+	public function check( $user_id = "", $media_id = "", $action = "" ) {
+	    if( $user_id == "" || $media_id == "" || $action == "" ){
+	        return false;
+		}
+		
+	    $columns = array(
+	        'user_id' => $user_id,
+	        'media_id' => $media_id,
+	        'action' => $action
+	    );
+		
+	    $results = $this->get( $columns );
+		
+	    if( $results ){
+	        return true;
+		} else {
+	        return false;
+		}
+	}
 
-    function get_row($user_id = "", $media_id = "", $action = "") {
-        if($user_id == "" || $media_id == "" || $action == "")
-            return false;
-        $columns = array(
-            'user_id' => $user_id,
-            'media_id' => $media_id,
-            'action' => $action
-        );
-        $results = $this->get($columns);
-        return $results;
-    }
+	/**
+	 * get_row.
+	 *
+	 * @access public
+	 * @param  int  $user_id
+	 * @param  int  $media_id
+	 * @param  type $action
+	 * @return type $results
+	 */
+	function get_row( $user_id = "", $media_id = "", $action = "" ) {
+	    if( $user_id == "" || $media_id == "" || $action == "" ){
+	        return false;
+		}
+		
+	    $columns = array(
+	        'user_id' => $user_id,
+	        'media_id' => $media_id,
+	        'action' => $action
+	    );
+		
+	    $results = $this->get( $columns );
+		
+	    return $results;
+	}
 }
