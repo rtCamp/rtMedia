cf3fb6527200990ca93741aa481ef6ca038a6823
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2014-07-29 14:35:21 +0530

    added function header

--- app/helper/RTMediaAdminWidget.php
+++ app/helper/RTMediaAdminWidget.php
@@ -5,31 +5,33 @@
  * @author Gagandeep Singh <gagandeep.singh@rtcamp.com>
  * @author Joshua Abenazer <joshua.abenazer@rtcamp.com>
  */
-if (!class_exists('RTMediaAdminWidget')) {
+if ( ! class_exists( 'RTMediaAdminWidget' ) ){
 
-    class RTMediaAdminWidget {
+	class RTMediaAdminWidget {
 
-        /**
-         *
-         * @global type $rtmedia
-         * @param type $id
-         * @param type $title
-         * @param type $content
-         */
-        public function __construct($id = NULL, $title = NULL, $content = NULL) {
-            global $rtmedia;
-            if ($id) {
-                ?>
-                <div class="postbox" id="<?php echo $id; ?>"><?php if ($title) { ?>
-                        <h3 class="hndle"><span><?php echo $title; ?></span></h3><?php }
-                ?>
-                    <div class="inside"><?php echo $content; ?></div>
-                </div><?php
-            } else {
-                trigger_error(__('Argument missing. id is required.', 'rtmedia'));
-            }
-        }
+		/**
+		 *
+		 * @global type  $rtmedia
+		 * @param  type  $id
+		 * @param  type  $title
+		 * @param  type  $content
+		 */
+		public function __construct( $id = NULL, $title = NULL, $content = NULL ) {
+			global $rtmedia;
+			if ( $id ){
+				?>
+				<div class="postbox" id="<?php echo $id; ?>">
+				<?php if ( $title ){ ?>
+						<h3 class="hndle"><span><?php echo $title; ?></span></h3>
+		        <?php }
+				?>
+				    <div class="inside"><?php echo $content; ?></div>
+				</div><?php
+			} else {
+				trigger_error( __( 'Argument missing. id is required.', 'rtmedia' ) );
+			}
+		}
 
-    }
+	}
 
 }
