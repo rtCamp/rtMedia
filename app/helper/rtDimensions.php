c06b972e5cb7966a158aac6187a2bd1b77df532e
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2014-07-31 11:24:14 +0530

    fixing documentation bug

--- app/helper/rtDimensions.php
+++ app/helper/rtDimensions.php
@@ -152,7 +152,7 @@ class rtDimensions extends rtForm {
 	 * generate_dimensions.
 	 *
 	 * @access public
-	 * @param  type  $attributes
+	 * @param  mixed  $attributes
 	 * @return void
 	 */
 	public function get_dimensions ( $attributes = '' ) {
