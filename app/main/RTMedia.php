ae8360d8599d0539223e432dcda25b6857c9596b
Sanket Parmar <sanket.parmar11@gmail.com>
2014-08-06 18:58:57 +0530

    rename function

--- app/main/RTMedia.php
+++ app/main/RTMedia.php
@@ -1024,7 +1024,7 @@ function get_rtmedia_permalink($id) {
     global $rtmedia_query;
 
     // Adding filter to get permalink for current blog
-    add_filter( 'bp_get_root_domain', 'rtmedia_get_current_blog_id');
+    add_filter( 'bp_get_root_domain', 'rtmedia_get_current_blog_url');
 
     if (!isset($media[0]->context)) {
         if (function_exists("bp_get_groups_root_slug") && isset($rtmedia_query->query) && isset($rtmedia_query->query["context"]) && $rtmedia_query->query["context"] == "group") {
@@ -1051,7 +1051,7 @@ function get_rtmedia_permalink($id) {
     $parent_link = trailingslashit($parent_link);
     
     // Removing filter so that doesn't affect other calls to this function
-    remove_filter( 'bp_get_root_domain', 'rtmedia_get_current_blog_id');
+    remove_filter( 'bp_get_root_domain', 'rtmedia_get_current_blog_url');
     
     return trailingslashit($parent_link . RTMEDIA_MEDIA_SLUG . '/' . $id);
 }
