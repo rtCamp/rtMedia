1d4d8209f662ccf9bc552d4efcf137619e87fa97
Ritesh Patel <ritesh.patel@rtcamp.com>
2014-08-07 18:52:03 +0530

    add template update admin notice

--- app/admin/RTMediaAdmin.php
+++ app/admin/RTMediaAdmin.php
@@ -1644,10 +1644,10 @@ if ( ! class_exists( 'RTMediaAdmin' ) ){
 		}
 
 		function rtmedia_update_template_notice() {
-			$site_option = rtmedia_get_site_option( "rtmedia-update-template-notice-v3_7_2" );
+			$site_option = rtmedia_get_site_option( "rtmedia-update-template-notice-v3_7_3" );
 
 			if ( ! $site_option || $site_option != "hide" ){
-				rtmedia_update_site_option( "rtmedia-update-template-notice-v3_7_2", "show" );
+				rtmedia_update_site_option( "rtmedia-update-template-notice-v3_7_3", "show" );
 				if ( is_dir( get_template_directory() . '/rtmedia' ) ){
 					echo '<div class="error rtmedia-update-template-notice"><p>' . __( 'Please update rtMedia template files if you have overridden the default rtMedia templates in your theme. If not, you can ignore and hide this notice.' ) . '<a href="#" onclick="rtmedia_hide_template_override_notice()" style="float:right">' . __( 'Hide', 'rtmedia' ) . '</a>' . ' </p></div>';
 					?>
@@ -1668,7 +1668,7 @@ if ( ! class_exists( 'RTMediaAdmin' ) ){
 
 		function rtmedia_hide_template_override_notice() {
 
-			if ( rtmedia_update_site_option( "rtmedia-update-template-notice-v3_7_2", "hide" ) ){
+			if ( rtmedia_update_site_option( "rtmedia-update-template-notice-v3_7_3", "hide" ) ){
 				echo "1";
 			} else {
 				echo "0";
