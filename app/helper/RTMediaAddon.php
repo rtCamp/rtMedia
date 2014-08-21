879d9d3003f9e01d5419fbdcaf045842f76ab8a4
Ritesh Patel <ritesh.patel@rtcamp.com>
2014-08-21 18:48:29 +0530

    chagne tab positon in addon page

--- app/helper/RTMediaAddon.php
+++ app/helper/RTMediaAddon.php
@@ -68,16 +68,17 @@ if ( ! class_exists( 'RTMediaAddon' ) ){
 			$tabs = array();
 			global $rtmedia_admin;
 			$tabs[] = array(
-				'title' => __( 'Audio/Video  Encoding', 'rtmedia' ),
-				'name' => __( 'Audio/Video  Encoding', 'rtmedia' ),
-				'href' => '#rtm-services',
-				'callback' => array( $rtmedia_admin->rtmedia_encoding, 'encoding_service_intro' )
-			);
-			$tabs[] = array(
 				'title' => __( 'Plugins', 'rtmedia' ),
 				'name' => __( 'Plugins', 'rtmedia' ),
 				'href' => '#rtm-plugins',
 				'callback' => array( $this, 'plugins_content' )
+			);
+
+			$tabs[] = array(
+				'title' => __( 'Audio/Video  Encoding', 'rtmedia' ),
+				'name' => __( 'Audio/Video  Encoding', 'rtmedia' ),
+				'href' => '#rtm-services',
+				'callback' => array( $rtmedia_admin->rtmedia_encoding, 'encoding_service_intro' )
 			);
 
 			/*			$tabs[] = array(
@@ -144,7 +145,7 @@ if ( ! class_exists( 'RTMediaAddon' ) ){
 						'title' => __( 'rtMedia Photo Watermark', 'rtmedia' ),
 						'img_src' => $img_src.'rtmedia-watermark-240x184.png',
 						'product_link' => 'http://rtcamp.com/store/rtmedia-photo-watermark/?utm_source=dashboard&utm_medium=plugin&utm_campaign=buddypress-media',
-						'desc' => '<p>' . __( 'rtMedia Photo Watermark add-on let you add text or copyright your images uploaded using rtMedia.', 'rtmedia' ) . '</p>
+						'desc' => '<p>' . __( 'rtMedia Photo Watermark add-on let you add text or copyright on your images uploaded using rtMedia.', 'rtmedia' ) . '</p>
 						<p><strong>' . __( 'Important', 'rtmedia' ) . ':</strong> ' . __( 'You need to have either ImageMagick or GD library installed on your server for this addon to work.', 'rtmedia' ) . '</p>',
 						'price' => '$49',
 						'demo_link' => 'http://demo.rtcamp.com/rtmedia/?utm_source=dashboard&utm_medium=plugin&utm_campaign=buddypress-media',
