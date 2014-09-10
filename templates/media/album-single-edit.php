2807c03b69044364e54c42f833f7d3ebbafe9655
Ritesh Patel <ritesh.patel@rtcamp.com>
2014-09-10 16:16:36 +0530

    add fix width icon class

--- templates/media/album-single-edit.php
+++ templates/media/album-single-edit.php
@@ -13,9 +13,9 @@ $rtmedia_media = $media[0];
 
         <div class="rtmedia-edit-media-tabs rtmedia-editor-main columns large-12 small">
             <dl class="tabs" data-tab>
-                <dd class="active"><a href="#details-tab"><i class='rtmicon-edit'></i><?php _e('Details', 'rtmedia') ;?></a></dd>
+                <dd class="active"><a href="#details-tab"><i class='rtmicon-edit rtmicon-fw'></i><?php _e('Details', 'rtmedia') ;?></a></dd>
                 <?php if(!is_rtmedia_group_album()) { ?>
-                <dd class=""><a href="#manage-media-tab"><i class='rtmicon-edit'></i><?php _e('Manage Media', 'rtmedia') ;?></a></dd>
+                <dd class=""><a href="#manage-media-tab"><i class='rtmicon-edit rtmicon-fw'></i><?php _e('Manage Media', 'rtmedia') ;?></a></dd>
                 <?php } ?>
                 <!-- use this hook to add title of a new tab-->
                 <?php do_action('rtmedia_add_edit_tab_title', 'album');?>
@@ -55,9 +55,9 @@ $rtmedia_media = $media[0];
                         <form class="rtmedia-album-edit rtmedia-bulk-actions" method="post" name="rtmedia_album_edit">
                             <?php wp_nonce_field('rtmedia_bulk_delete_nonce', 'rtmedia_bulk_delete_nonce'); ?>
                             <?php RTMediaMedia::media_nonce_generator($rtmedia_query->media_query['album_id']); ?>
-                            <button type='button' class="select-all" title="<?php echo __('Select All Visible','rtmedia'); ?>"><i class='rtmicon-square-o'></i></button>
-                            <button class="button rtmedia-move" type='button' title='<?php echo __('Move Selected media to another album.');?>' ><i class='rtmicon-move'></i> <?php _e('Move','rtmedia'); ?></button>
-                            <button type="button" name="delete-selected" class="button rtmedia-delete-selected" title='<?php echo __('Delete Selected media from the album.');?>'><i class='rtmicon-trash'></i><?php _e('Delete','rtmedia'); ?></button>
+                            <button type='button' class="select-all" title="<?php echo __('Select All Visible','rtmedia'); ?>"><i class='rtmicon-square-o rtmicon-fw'></i></button>
+                            <button class="button rtmedia-move" type='button' title='<?php echo __('Move Selected media to another album.');?>' ><i class='rtmicon-move rtmicon-fw'></i> <?php _e('Move','rtmedia'); ?></button>
+                            <button type="button" name="delete-selected" class="button rtmedia-delete-selected" title='<?php echo __('Delete Selected media from the album.');?>'><i class='rtmicon-trash rtmicon-fw'></i><?php _e('Delete','rtmedia'); ?></button>
                             <div class="rtmedia-move-container">
                                 <?php $global_albums = rtmedia_get_site_option('rtmedia-global-albums'); ?>
                                 <?php _e('Move selected media to the album : ', 'rtmedia'); ?>
--- templates/media/media-single-edit.php
+++ templates/media/media-single-edit.php
@@ -16,7 +16,7 @@
             <form method="post" action="" name="rtmedia_media_single_edit" id="rtmedia_media_single_edit">
                 <div class="rtmedia-editor-main columns large-12 small">
                     <dl class="tabs" data-tab>
-                        <dd class="active"><a href="#panel1"><i class='rtmicon-edit'></i><?php _e('Details', 'rtmedia') ;?></a></dd>
+                        <dd class="active"><a href="#panel1"><i class='rtmicon-edit rtmicon-fw'></i><?php _e('Details', 'rtmedia') ;?></a></dd>
                         <!-- use this hook to add title of a new tab-->
                         <?php do_action('rtmedia_add_edit_tab_title', rtmedia_type());?>
                     </dl>
