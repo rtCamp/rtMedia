9444a5e869b2f3c1acacf2c79b1231676b36420b
Ritesh Patel <ritesh.patel@rtcamp.com>
2014-09-18 21:07:15 +0530

    add addon license

---
+++ app/helper/RTMediaLicense.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ritz
+ * Date: 18/9/14
+ * Time: 5:05 PM
+ */
+
+class RTMediaLicense {
+
+	static function render_themes( $page = '' ){
+		global $wp_actions;
+
+		if( has_action( 'rtmedia_addon_license_details' ) ){
+		?>
+		<div id="rtm-licenses">
+		<?php
+		}
+
+		do_action( 'rtmedia_addon_license_details' );
+
+		if( has_action( 'rtmedia_addon_license_details' ) ){
+		?>
+			</div>
+		<?php
+		}
+	}
+
+} 
