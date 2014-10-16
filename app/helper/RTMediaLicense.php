27e69dc6813190d6c7e86f9320d666b115fbf5b8
Ritesh Patel <ritesh.patel@rtcamp.com>
2014-10-16 19:56:02 +0530

    fix phpcs warning in RTMediaLicense.php

--- app/helper/RTMediaLicense.php
+++ app/helper/RTMediaLicense.php
@@ -1,32 +1,32 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: ritz
  * Date: 18/9/14
  * Time: 5:05 PM
  */
-
 class RTMediaLicense {
 
 	static function render_themes( $page = '' ){
 		global $wp_actions;
 
-		if( has_action( 'rtmedia_addon_license_details' ) ){
-		?>
-		<div id="rtm-licenses">
+		if ( has_action( 'rtmedia_addon_license_details' ) ){
+			?>
+			<div id="rtm-licenses">
 		<?php
 		}
 
 		do_action( 'rtmedia_addon_license_details' );
 
-		if( has_action( 'rtmedia_addon_license_details' ) ){
-		?>
+		if ( has_action( 'rtmedia_addon_license_details' ) ){
+			?>
 			</div>
 		<?php
 		} else {
-	?>
+			?>
 			<div>You may be interested in <a href="<?php echo admin_url( 'admin.php?page=rtmedia-addons' ) ?>">rtMedia Addons</a>.</div>
-	<?php
+		<?php
 		}
 	}
 
