1b294537e3910ce9610dc61987f391bb5b035068
Ritesh Patel <ritesh.patel@rtcamp.com>
2014-11-03 19:03:38 +0530

    fix phpcs warnigns in RTMediaGroup.php

--- app/main/controllers/group/RTMediaGroup.php
+++ app/main/controllers/group/RTMediaGroup.php
@@ -1,33 +1,32 @@
 <?php
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  *
  */
-class RTMediaGroup{
-    public $create_slug = "media-setting";
-    function __construct() {
-	global $rtmedia;
-	$options = $rtmedia->options;
-	if(  isset ( $options [ 'buddypress_enableOnGroup' ] ) && $options [ 'buddypress_enableOnGroup' ] == "1" ) {
-	   // return;
-            $extension = true;
-            if( isset($options['general_enableAlbums']) && $options['general_enableAlbums'] == 0){
-                $extension = false;
-            }
-            $extension = apply_filters('rtmedia_group_media_extension', $extension);
-            if(!$extension){
-                return;
-            }
-            
-        }else {
-            return;
-        }
-        
-        if(function_exists("bp_is_active") &&  bp_is_active("groups")){
-            bp_register_group_extension("RTMediaGroupExtension");
-        }
 
-    }
+class RTMediaGroup {
+	public $create_slug = 'media-setting';
 
+	function __construct(){
+		global $rtmedia;
+		$options = $rtmedia->options;
+		if ( isset( $options['buddypress_enableOnGroup'] ) && '1' == $options ['buddypress_enableOnGroup'] ){
+			// return;
+			$extension = true;
+			if ( isset( $options['general_enableAlbums'] ) && 0 == $options['general_enableAlbums'] ){
+				$extension = false;
+			}
+			$extension = apply_filters( 'rtmedia_group_media_extension', $extension );
+			if ( ! $extension ){
+				return;
+			}
+		} else {
+			return;
+		}
+		if ( function_exists( 'bp_is_active' ) && bp_is_active( 'groups' ) ){
+			bp_register_group_extension( 'RTMediaGroupExtension' );
+		}
+	}
 }
