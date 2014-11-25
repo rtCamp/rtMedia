3570f710c50fb2693919aa84a96c8ddd52fba8e6
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2014-11-25 14:51:10 +0530

    fixed using pgp-cs editor

--- app/main/controllers/privacy/RTMediaFriends.php
+++ app/main/controllers/privacy/RTMediaFriends.php
@@ -15,32 +15,47 @@ class RTMediaFriends {
 	/**
 	 *
 	 */
-	function __construct() {
-		if(!class_exists('BuddyPress')) return;
-		if(!bp_is_active('friend'))return;
-		add_action('friends_friendship_accepted',array($this,'refresh_friends_cache'));
-		add_action('friends_friendship_deleted',array($this,'refresh_friends_cache'));
+	function __construct(){
+		if ( ! class_exists( 'BuddyPress' ) ){
+			return;
+		}
+		if ( ! bp_is_active( 'friend' ) ){
+			return;
+		}
+		add_action( 'friends_friendship_accepted', array( $this, 'refresh_friends_cache' ) );
+		add_action( 'friends_friendship_deleted', array( $this, 'refresh_friends_cache' ) );
 	}
 
-	function get_friends_cache( $user ) {
+	function get_friends_cache( $user ){
 
-		if(!class_exists('BuddyPress')) return array();
-		if(!bp_is_active('friends'))return array();
-
-		if ( ! $user )
+		if ( ! class_exists( 'BuddyPress' ) ){
 			return array();
-		$friends = wp_cache_get( 'rtmedia-user-friends-' . $user );
-		if ( $friends === false ) {
-			$friends = $this->refresh_friends_cache($user);
 		}
+		if ( ! bp_is_active( 'friends' ) ){
+			return array();
+		}
+
+		if ( ! $user ){
+			return array();
+		}
+		$friends = wp_cache_get( 'rtmedia-user-friends-' . $user );
+		if ( $friends === false ){
+			$friends = $this->refresh_friends_cache( $user );
+		}
+
 		return $friends;
 	}
 
-	function refresh_friends_cache($user){
-		if(!class_exists('BuddyPress')) return;
-		if(!bp_is_active('friends'))return;
-		$friends = friends_get_friend_user_ids($user);
+	function refresh_friends_cache( $user ){
+		if ( ! class_exists( 'BuddyPress' ) ){
+			return;
+		}
+		if ( ! bp_is_active( 'friends' ) ){
+			return;
+		}
+		$friends = friends_get_friend_user_ids( $user );
 		wp_cache_set( 'rtmedia-user-friends-' . $user, $friends );
+
 		return $friends;
 	}
 
