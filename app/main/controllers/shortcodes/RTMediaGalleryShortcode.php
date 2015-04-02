f9fc87b309135f4c744aca766e8837a860c2b804
Pranali Patel <pranali.patel@rtcamp.com>
2015-04-02 19:21:46 +0530

    Added variable for rtmedia_slug

--- app/main/controllers/shortcodes/RTMediaGalleryShortcode.php
+++ app/main/controllers/shortcodes/RTMediaGalleryShortcode.php
@@ -48,10 +48,11 @@ class RTMediaGalleryShortcode {
         }
         wp_localize_script ( 'rtmedia-backbone', 'template_url', $template_url );
     	$url = trailingslashit ( $_SERVER[ "REQUEST_URI" ] );
+		$rtmedia_slug = "/".RTMEDIA_MEDIA_SLUG;
         // check position of media slug from end of the URL
-		if ( strrpos ( $url, "/".RTMEDIA_MEDIA_SLUG ) !== false ) {
+		if ( strrpos ( $url, $rtmedia_slug ) !== false ) {
 			// split the url upto the last occurance of media slug
-			$url_upload = substr( $url, 0, strrpos ( $url, "/".RTMEDIA_MEDIA_SLUG ) );
+			$url_upload = substr( $url, 0, strrpos ( $url, $rtmedia_slug ) );
 			$url = trailingslashit ( $url_upload ) . "upload/";
         } else {
             $url = trailingslashit ( $url ) . "upload/";
