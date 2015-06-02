09fa128e6a453d81670a9148397c7e445968d967
sanket <sanket.parmar@rtcamp.com>
2015-06-02 13:00:23 +0530

    Admin notice for rtMedia Pro Split

--- app/admin/RTMediaAdmin.php
+++ app/admin/RTMediaAdmin.php
@@ -104,6 +104,7 @@ if ( ! class_exists( 'RTMediaAdmin' ) ) {
 			add_action( 'admin_init', array( $this, 'rtmedia_bp_add_update_type' ) );
 			add_action( 'wp_ajax_rtmedia_hide_inspirebook_release_notice', array( $this, 'rtmedia_hide_inspirebook_release_notice' ), 1 );
 			add_action( 'wp_ajax_rtmedia_hide_social_sync_notice', array( $this, 'rtmedia_hide_social_sync_notice' ), 1 );
+            add_action( 'wp_ajax_rtmedia_hide_pro_split_notice', array( $this, 'rtmedia_hide_pro_split_notice' ), 1 );
 			$rtmedia_media_import = new RTMediaMediaSizeImporter(); // do not delete this line. We only need to create object of this class if we are in admin section
 			if ( class_exists( 'BuddyPress' ) ) {
 				$rtmedia_activity_upgrade = new RTMediaActivityUpgrade();
@@ -154,7 +155,53 @@ if ( ! class_exists( 'RTMediaAdmin' ) ) {
 				$this->rtmedia_update_template_notice();
 				$this->rtmedia_inspirebook_release_notice();
 				$this->rtmedia_social_sync_release_notice();
+                $this->rtmedia_pro_split_release_notice();
 			}
+		}
+        
+        /*
+         * rtMedia Pro split release admin notice
+         */
+        public function rtmedia_pro_split_release_notice() {
+            $site_option = rtmedia_get_site_option( 'rtmedia_pro_split_release_notice' );
+			
+			if( ( !$site_option || 'hide' != $site_option ) ) {
+				rtmedia_update_site_option( 'rtmedia_pro_split_release_notice', 'show' );
+				?>
+				<div class="updated rtmedia-pro-split-notice">
+					<p>
+						<span>
+                            <b>rtMedia: </b>We have released 30+ premium add-ons for rtMedia plugin. Read more about it <a href="https://rtcamp.com/blog/rtmedia-pro-splitting-major-change/?utm_source=dashboard&utm_medium=plugin&utm_campaign=buddypress-media" target="_blank">here</a>.
+						</span>
+						<a href="#" onclick="rtmedia_hide_pro_split_notice();" style="float:right">Dismiss</a>
+					</p>
+				</div>
+				<script type="text/javascript">
+					function rtmedia_hide_pro_split_notice() {
+						var data = { action: 'rtmedia_hide_pro_split_notice' };
+						jQuery.post( ajaxurl, data, function ( response ) {
+							response = response.trim();
+							
+                            if( response === "1" )
+								jQuery( '.rtmedia-pro-split-notice' ).remove();
+						} );
+					}
+				</script>
+				<?php
+			}
+        }
+        
+        /*
+		 * Hide pro split release notice
+		 */
+
+		function rtmedia_hide_pro_split_notice() {
+			if ( rtmedia_update_site_option( 'rtmedia_pro_split_release_notice', 'hide' ) ) {
+				echo '1';
+			} else {
+				echo '0';
+			}
+			die();
 		}
 
 		/*
