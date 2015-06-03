524b25975653844fd869701b869da0f993db7c17
sanket <sanket.parmar@rtcamp.com>
2015-06-03 15:13:37 +0530

    Show login popup if user is not logged in on Upload click

--- app/main/controllers/template/rt-template-functions.php
+++ app/main/controllers/template/rt-template-functions.php
@@ -2143,6 +2143,35 @@ function show_rtmedia_like_counts() {
 	}
 }
 
+add_action( 'rtmedia_album_gallery_actions', 'rtmedia_add_upload_album_button', 99 );
+add_action( 'rtmedia_media_gallery_actions', 'rtmedia_add_upload_album_button', 99 );
+function rtmedia_add_upload_album_button() {
+	if ( ! is_user_logged_in() ){
+		echo '<span><a href="#rtmedia-login-register-modal" class="primary rtmedia-upload-media-link rtmedia-modal-link" id="rtmedia-login-register-modal" title="' . __( 'Upload Media', 'rtmedia' ) . '"><i class="dashicons dashicons-upload rtmicon"></i>' . __( 'Upload' ) . '</a></span>';
+	}
+}
+
+add_action( 'rtmedia_before_media_gallery', 'rtmedia_login_register_modal' );
+add_action( 'rtmedia_before_album_gallery', 'rtmedia_login_register_modal' );
+function rtmedia_login_register_modal() {
+
+	if ( ! is_user_logged_in() ){
+		?>
+		<div class="rtmedia-popup mfp-hide rtm-modal" id="rtmedia-login-register-modal">
+			<div id="rtm-modal-container">
+				<h2 class="rtm-modal-title"><?php _e( 'Please login', 'rtmedia' ); ?></h2>
+
+				<p><?php _e( "You need to be logged in to upload Media or to create Album.", 'rtmedia' ); ?></p>
+
+				<p>
+					<?php echo __( 'Click ' ) . '<a href="' . wp_login_url() . '" title="' . __( 'Login', 'rtmedia' ) . '">' . __( 'HERE', 'rtmedia' ) . '</a>' . __( ' to login.', 'rtmedia' ); ?>
+				</p>
+			</div>
+		</div>
+	<?php
+	}
+}
+
 add_action( 'rtmedia_media_gallery_actions', 'add_upload_button', 99 );
 add_action( 'rtmedia_album_gallery_actions', 'add_upload_button', 99 );
 
