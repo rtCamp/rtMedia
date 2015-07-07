8419a3fa2edede1c729cc3bd1bca57a2f0532064
Ritesh Patel <ritzpatel91@gmail.com>
2015-07-07 20:04:01 +0530

    fix privacy dropdown

--- app/main/controllers/upload/RTMediaUploadView.php
+++ app/main/controllers/upload/RTMediaUploadView.php
@@ -56,7 +56,10 @@ class RTMediaUploadView {
 			}
 		}
         $up_privacy = $privacy = ""; //uploader privacy dropdown for uploader under rtMedia Media tab.
-		if ( is_rtmedia_privacy_enable() && ( isset( $rtmedia_query->is_upload_shortcode ) && ! isset( $this->attributes['privacy'] ) )  ) {
+		if ( is_rtmedia_privacy_enable()
+			&& ( ( ! isset( $rtmedia_query->is_upload_shortcode ) || $rtmedia_query->is_upload_shortcode === false) )
+				|| ( isset( $rtmedia_query->is_upload_shortcode ) && ! isset( $this->attributes['privacy'] ) )
+		) {
 			if ( ( isset( $rtmedia_query->query[ 'context' ] ) && $rtmedia_query->query[ 'context' ] == 'group' ) || ( function_exists( 'bp_is_groups_component' ) && bp_is_groups_component() ) ) {
                 // if the context is group, then set the media privacy to public
                 $privacy = "<input type='hidden' name='privacy' value='0'/>";
