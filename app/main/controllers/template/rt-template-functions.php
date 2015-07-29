a8c4b38e2e8b8e366a6f74725952d5e91530f3c1
Sanket <sanket.parmar@rtcamp.com>
2015-07-29 16:44:01 +0530

    Updated function name of script_debug constant

--- app/main/controllers/template/rt-template-functions.php
+++ app/main/controllers/template/rt-template-functions.php
@@ -2726,7 +2726,7 @@ function rtm_filter_metaid_column_name( $q ) {
 /*
  * Checking if SCRIPT_DEBUG constant is defined or not
  */
-function rtm_check_script_debug_constant() {
+function rtm_get_script_style_suffix() {
 	$suffix = ( defined( 'SCRIPT_DEBUG' ) && constant( 'SCRIPT_DEBUG' ) === true ) ? '' : '.min';
 
 	return $suffix;
