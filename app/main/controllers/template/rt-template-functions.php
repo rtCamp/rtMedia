71af8e75fb4aa6cad7aa6eb84a4f18143ba9aa0f
Jignesh <jignesh.nakrani@ymail.com>
2015-08-14 13:40:53 +0530

    added condition to get video custom thumbnail

--- app/main/controllers/template/rt-template-functions.php
+++ app/main/controllers/template/rt-template-functions.php
@@ -362,6 +362,9 @@ function rtmedia_image( $size = 'rt_media_thumbnail', $id = false, $recho = true
 	$thumbnail_id = 0;
 	if ( isset( $media_object->media_type ) ) {
 		if ( $media_object->media_type == 'album' || $media_object->media_type != 'photo' || $media_object->media_type == 'video' ) {
+			if ( $media_object->media_type == 'video' ) {
+				$thumbnail_id = ( isset( $media_object->cover_art ) && ( $media_object->cover_art != "0" ) ) ? $media_object->cover_art : false;
+			}
 			$thumbnail_id = apply_filters( 'show_custom_album_cover', $thumbnail_id, $media_object->media_type, $media_object->id ); // for rtMedia pro users
 		} elseif ( $media_object->media_type == 'photo' ) {
 			$thumbnail_id = $media_object->media_id;
