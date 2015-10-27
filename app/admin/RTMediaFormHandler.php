b4d4c8730ff891d0a8e2a4c479a9a4ef5c2664f8
Ritesh Patel <ritzpatel91@gmail.com>
2015-10-27 15:38:20 +0530

    update settings labels

--- app/admin/RTMediaFormHandler.php
+++ app/admin/RTMediaFormHandler.php
@@ -1007,12 +1007,12 @@ class RTMediaFormHandler {
 				'group' => 10,
 			),
                         'rtmedia-enable-notification' => array(
-				'title' => __( 'Enable media notification for rtMedia', 'rtmedia' ),
+				'title' => __( 'Enable media notification', 'rtmedia' ),
 				'callback' => array( 'RTMediaFormHandler', 'checkbox' ),
 				'args' => array(
 					'key' => 'buddypress_enableNotification',
 					'value' => $options[ 'buddypress_enableNotification' ],
-					'desc' => __( 'This will enable notification for media to user for likes and comments.', 'rtmedia' ),
+					'desc' => __( 'This will enable notifications to media authors for media likes and comments.', 'rtmedia' ),
 					
 				),
 				'group' => 10,
