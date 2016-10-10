73566234353a0a091c2b59a3159f72dae23d728c
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2016-10-10 18:39:39 +0530

    coding standart

--- app/main/controllers/template/rtmedia-actions.php
+++ app/main/controllers/template/rtmedia-actions.php
@@ -558,12 +558,12 @@ function rtmedia_add_media_delete_nonce_shortcode() {
 	}
 
 }
+add_action( 'rtmedia_pre_template', 'rtmedia_add_media_delete_nonce_shortcode' );
 
 /**
  * add function to display pagination on single media page with add_filter
  * By: Yahil
  */
-add_action( 'rtmedia_pre_template', 'rtmedia_add_media_delete_nonce_shortcode' );
 
 if ( ! function_exists( 'rtmedia_single_media_pagination' ) ) {
 	function rtmedia_single_media_pagination() {
