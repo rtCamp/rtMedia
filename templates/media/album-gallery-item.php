8480b910cd6a3dd70a75b276bbc9b2d66a78b9df
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2016-10-10 12:32:43 +0530

    display album count

--- templates/media/album-gallery-item.php
+++ templates/media/album-gallery-item.php
@@ -21,20 +21,31 @@ if ( isset( $is_edit_allowed[0] ) ) {
 	$rtmedia_backbone['is_edit_allowed'] = $is_edit_allowed[0];
 }
 ?>
-<!-- addClass if not default album -->
 <li class="rtmedia-list-item">
-
 	<a href="<?php rtmedia_permalink(); ?>" title="<?php echo esc_attr( rtmedia_title() ); ?>">
-
 		<div class="rtmedia-item-thumbnail">
 			<img src="<?php rtmedia_image( 'rt_media_thumbnail' ); ?>" alt="<?php echo esc_attr( rtmedia_title() ); ?>">
 		</div>
 
-		<?php if ( apply_filters( 'rtmedia_media_gallery_show_media_title', true ) ) { ?>
+		<?php
+		if ( apply_filters( 'rtmedia_media_gallery_show_media_title', true ) ) {
+
+			/**
+			 * Add Album Count On Album Thumbnail
+			 * By: Yahil
+			 */
+			if ( bp_is_group() ) {
+				$context = 'group';
+			} else {
+				$context = 'profile';
+			}
+			?>
 			<div class="rtmedia-item-title">
-				<h4><?php echo esc_html( rtmedia_title() ); ?></h4>
+			  <h4><?php echo esc_html( rtmedia_title() ); ?><span>(<?php echo esc_html( rtmedia_album_count( rtmedia_id(), $context  ,bp_displayed_user_id() ) ); ?></span>)</h4>
 			</div>
-		<?php } ?>
+			<?php
+		}
+	 	?>
 
 	</a>
 	<?php do_action( 'rtmedia_after_album_gallery_item' ) ?>
