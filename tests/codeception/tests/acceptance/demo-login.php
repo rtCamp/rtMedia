5b4d63c9939787fe15e451819c82cf238e005e80
sumeet <sumeet.sarna@rtcamp.com>
2016-10-11 23:15:13 +0530

    Added tests for uploading an image in buddypress activity and uplaoding via user profile page.

---
+++ tests/codeception/tests/acceptance/demo-login.php
@@ -0,0 +1,10 @@
+<?php
+// Login to the demo site
+$I = new AcceptanceTester($scenario);
+$I->wantTo('Upload an image at profile page');
+$I->amonPage('/');
+$I->fillField( 'input#bp-login-widget-user-login', 'demo' );
+$I->fillField( 'input#bp-login-widget-user-pass', 'demo' );
+$I-> click('Log In');
+
+?>
