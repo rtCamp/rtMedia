e426a9912e406132e51966ae8c5aa6301f559816
sumeet <sumeet.sarna@rtcamp.com>
2016-10-11 21:47:37 +0530

    Added demo login code in a separate file. Added a new file using which user can upload an image after checking terms and conditions in media page

---
+++ tests/codeception/tests/acceptance/demo-login.php
@@ -0,0 +1,9 @@
+<?php
+$I = new AcceptanceTester($scenario);
+$I->wantTo('Upload an image at profile page');
+$I->amonPage('/');
+$I->fillField( 'input#bp-login-widget-user-login', 'demo' );
+$I->fillField( 'input#bp-login-widget-user-pass', 'demo' );
+$I-> click('Log In');
+
+?>
--- tests/codeception/tests/acceptance/display-comment-imageCept.php
+++ tests/codeception/tests/acceptance/display-comment-imageCept.php
@@ -1,19 +1,15 @@
-<?php
+<?php 
 /*  
 Prerequisites:
 
 For this test to pass. Buddypress and rtMedia should be installed and activated. BuddyPress activity page needs to be at yoursite.com/activity. rtMedia Settings > Display > Allow users to comment on uploaded media should be ON. rtMedia Settings > BuddyPress > Allow upload from activity stream should be ON.
 
 */
-
 $I = new AcceptanceTester($scenario);
-$I->wantTo('upload an image and comment on it');
-
-$I->amonPage('/wp-login.php');
-$I->fillField( 'input#user_login', 'sumeet' );
-$I->fillField( 'input#user_pass', 'sumeet' );
-$I-> click('Log In');
-$I->amonPage('/activity');
+$I->wantTo('perform actions and see result');
+$I->wantTo( 'Upload an image in buddypress activity' );
+require('demo-login.php');
+$I->see( "What's new, Demo?" );
 $I->click('#whats-new');
 $I->fillfield('#whats-new','Upload test');
 $I->attachFile('input[type="file"]','hello.png');
@@ -25,10 +21,11 @@ $I->wait(2);
 
 // Add a comment
 $I->click('.rtmedia-item-thumbnail');
-$I->wait(9);
+$I->wait(15);
 $I->click('textarea#comment_content');
-$I->fillField( 'textarea#comment_content', 'This is a test comment by sumeet' );
+$I->fillField( 'textarea#comment_content', 'This is a test comment' );
 $I->click('input#rt_media_comment_submit');
 $I->wait(4);
 
 ?>
+
