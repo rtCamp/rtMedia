6c8a7a95bdc049f3106dd76decc0c73b2a575333
naveen.giri <naveen.giri@rtcamp.com>
2016-10-13 12:51:09 +0530

    Fix duplicate activity generation Issue #108

--- app/assets/js/rtMedia.backbone.js
+++ app/assets/js/rtMedia.backbone.js
@@ -719,8 +719,13 @@ jQuery( function( $ ) {
 
 jQuery( document ).ready( function( $ ) {
 
-	//Handling the "post update: button on activity page
-	jQuery( '#aw-whats-new-submit' ).removeAttr( 'disabled' );
+	// Handling the "post update: button on activity page
+	/**
+	 * Commented by : Naveen giri
+	 * Reason : Commenting this code because its overriding buddypress functionality
+	 * 			and introducing issue Duplicate activity generation  Issue #108.
+	 */
+	/*JQuery( '#aw-whats-new-submit' ).removeAttr( 'disabled' );
 	jQuery( document ).on( 'blur', '#whats-new', function() {
 		setTimeout( function() {
 			jQuery( '#aw-whats-new-submit' ).removeAttr( 'disabled' );
@@ -730,7 +735,7 @@ jQuery( document ).ready( function( $ ) {
 		setTimeout( function() {
 			jQuery( '#aw-whats-new-submit' ).removeAttr( 'disabled' );
 		}, 100 );
-	} );
+	} );*/
 
 	// When user changes the value in activity "post in" dropdown, hide the privacy dropdown and show when posting in profile.
 	jQuery( '#whats-new-post-in' ).on( 'change', function( e ) {
