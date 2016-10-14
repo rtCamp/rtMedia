d8800003490f4a47faac56b85c6d20318cada938
krupa <krupananda.it@gmail.com>
2016-10-14 18:18:32 +0530

    Added reusable componet for login and upload media in AcceptanceTester.php file and Refectored test case - AllowUserToCommentOnUploadedMediaCept to fetch dynammic element from li tag

--- tests/codeception/tests/acceptance/AllowUserToCommentOnUploadedMediaCept.php
+++ tests/codeception/tests/acceptance/AllowUserToCommentOnUploadedMediaCept.php
@@ -1,7 +1,7 @@
 /*
 Scenario : To Allow the user to comment on uploaded media.
 Pre-requisite : In backend - Goto rtMedia settings -> Disaply -> SINGLE MEDIA VIEW ->
-Allow user to comment on uploaded media. This option must be on
+Allow user to comment on uploaded media. Assuming this option is on.
 */
 
 <?php
@@ -9,15 +9,17 @@ Allow user to comment on uploaded media. This option must be on
     $I = new AcceptanceTester($scenario);
     $I->wantTo('To check if the user is allwed to comment on uploaded media');
 
-    require('demo-login.php');
+    $I->login('demo','demo'); //Login to site
 
-    $I->amonPage('/members/demo/media/6761/'); //This will redirect the user to specific single media page.
-    $I->wait(5);
+    $I->uploadPhoto(); //Upload Photo
 
-    $I->seeElement('.rt_media_comment_form'); //Check if the comment form is visible
-    $I->fillfield('#comment_content',"This is test comment!"); //Add you comment here
-    $I->click('.rt_media_comment_submit'); 
+    $I->click('ul.rtm-gallery-list li:first-child'); //This will click on the fisrt child element
+
     $I->wait(5);
-    $I->see('This is test comment!');
+    $I->seeElement('#comment_content'); //Check if the comment form is visible
+    $I->fillfield('#comment_content',"test comment using script!"); //Add you comment here
+    $I->click('.rt_media_comment_submit');
+    $I->wait(5);
+    $I->see('test comment using script!');
 
 ?>
