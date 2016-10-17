7ed0faf95612ab07c68184544fe8d253c86d65b2
krupa <krupananda.it@gmail.com>
2016-10-17 11:22:36 +0530

    Added comment insdie php tag as per standard

--- tests/codeception/tests/acceptance/AllowUserToCommentOnUploadedMediaCept.php
+++ tests/codeception/tests/acceptance/AllowUserToCommentOnUploadedMediaCept.php
@@ -1,29 +1,28 @@
-/*
-Scenario : To Allow the user to comment on uploaded media.
-Pre-requisite : In backend - Goto rtMedia settings -> Display -> SINGLE MEDIA VIEW ->
-Allow user to comment on uploaded media. Assuming this option is on.
+<?php
+
+/**
+* Scenario : To Allow the user to comment on uploaded media.
+* Pre-requisite : In backend - Goto rtMedia settings -> Display -> SINGLE MEDIA VIEW -> Allow user to comment on uploaded media. Assuming this option is on.
 */
 
+          $userName = 'demo';
+          $password = 'demo';
 
-<?php
-        $userName = 'demo';
-        $password = 'demo';
+          $I = new AcceptanceTester($scenario);
+          $I->wantTo('To check if the user is allwed to comment on uploaded media');
 
-        $I = new AcceptanceTester($scenario);
-        $I->wantTo('To check if the user is allwed to comment on uploaded media');
+          $I->login($userName,$password); //Login to site with demo
 
-        $I->login($userName,$password); //Login to site with demo
+          $I->uploadPhoto($userName); //Upload Photo
 
-        $I->uploadPhoto($userName); //Upload Photo
+          $I->click('ul.rtm-gallery-list li:first-child'); //This will click on the fisrt child element
+          $I->wait(5);
+          $I->seeElement('#comment_content'); //Check if the comment form is visible
+          $I->fillfield('#comment_content',"test comment using script!"); //Add you comment here
+          $I->click('.rt_media_comment_submit');
+          $I->wait(5);
+          $I->see('test comment using script!');
 
-        $I->click('ul.rtm-gallery-list li:first-child'); //This will click on the fisrt child element
-        $I->wait(5);
-        $I->seeElement('#comment_content'); //Check if the comment form is visible
-        $I->fillfield('#comment_content',"test comment using script!"); //Add you comment here
-        $I->click('.rt_media_comment_submit');
-        $I->wait(5);
-        $I->see('test comment using script!');
-
-        $I->reloadPage();
-        $I->wait(5);
+          $I->reloadPage();
+          $I->wait(5);
 ?>
