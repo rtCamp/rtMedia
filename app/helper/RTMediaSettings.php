6ca342855bb656a24f46a2fb0415aff1cecff73d
yahil <yahil.madakiya@rtcamp.com>
2017-03-21 16:57:29 +0530

    Fix/GL #831: Set default disable media_search option.

--- app/helper/RTMediaSettings.php
+++ app/helper/RTMediaSettings.php
@@ -44,7 +44,7 @@ if ( ! class_exists( 'RTMediaSettings' ) ) {
 
 			$defaults = array(
 				'general_enableAlbums'				=> 1,
-				'general_enableGallerysearch'		=> 1,
+				'general_enableGallerysearch'		=> 0,
 				'general_enableAlbums_description'	=> 0,
 				'general_enableComments'			=> 0,
 				'general_enableLikes'				=> 1,
