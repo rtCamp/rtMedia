9e8b438e6ecabfa96bd995b5fddc28b3427db8f5
krupa <krupananda.it@gmail.com>
2017-03-23 18:15:54 +0530

    Removed functional, integration and unit directories and respecitve php files from Helper directory as they are not needed

--- tests/codeception/tests/_support/Helper/Acceptance.php
+++ tests/codeception/tests/_support/Helper/Acceptance.php
@@ -7,25 +7,4 @@ namespace Helper;
 class Acceptance extends \Codeception\Module
 {
 
-    public function uploadMediaFromActivity(){
-        $I = $this;
-        $I->click('#whats-new');
-        $I->waitForElementVisible('.rtmedia-add-media-button',2);
-        $I->click('.rtmedia-add-media-button');
-        $I->pressKey('.rtmedia-add-media-button',array('command','tab'));
-        $I->wait(3);
-        $I->pressKey('.rtmedia-add-media-button',array('shift','command','g'));
-        $I->sendKeys('/Users/javalnanda/Desktop/1.jpeg');
-        $I->pressKey('.rtmedia-add-media-button',\Facebook\WebDriver\WebDriverKeys::ENTER);
-        $I->wait(5);
-
-        // $I->pressKey('#page','a'); // => olda
-        // $I->pressKey('#page',array('ctrl','a'),'new'); //=> new
-        // $I->pressKey('#page',array('shift','111'),'1','x'); //=> old!!!1x
-        // $I->pressKey('descendant-or-self::*[ * `id='page']','u');
-        // $I->pressKey('#name', array('ctrl', 'a'), \Facebook\WebDriver\WebDriverKeys::DELETE);
-
-    }
-
-
 }
