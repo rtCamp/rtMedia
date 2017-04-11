006f772645d73380f5b16703dc8b8f37f7c00530
krupa <krupananda.it@gmail.com>
2017-04-11 14:56:16 +0530

    Create env var on travis repo Add relavant code in bootstrap file to access env var Update acceptance.suite.yml file update travis.yml file

--- tests/codeception/tests/_bootstrap.php
+++ tests/codeception/tests/_bootstrap.php
@@ -1,2 +1,8 @@
 <?php
 // This is global bootstrap for autoloading
+\Codeception\Configuration::$defaultSuiteSettings['modules']['config'] = [
+    'WPWebDriver' => [
+        'host' => 'hub-cloud.browserstack.com',
+        'access_key' => getenv('BROWSER_STACK_ACCESS_KEY'),
+    ]
+];
