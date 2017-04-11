aa0cffebd15e107b2aad132c74c86b74aa2e2b00
krupa <krupananda.it@gmail.com>
2017-04-11 15:39:51 +0530

    Remove code from bootstrap

--- tests/codeception/tests/_bootstrap.php
+++ tests/codeception/tests/_bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 // This is global bootstrap for autoloading
-\Codeception\Configuration::$defaultSuiteSettings['modules']['config'] = [
-    'WPWebDriver' => [
-        'host' => 'hub-cloud.browserstack.com',
-        'browserstack.user' => getenv( 'BROWSER_STACK_USERNAME' ),
-        'browserstack.key' => getenv( 'BROWSER_STACK_ACCESS_KEY' )
-    ]
-];
+// \Codeception\Configuration::$defaultSuiteSettings['modules']['config'] = [
+//     'WPWebDriver' => [
+//         'host' => 'hub-cloud.browserstack.com',
+//         'browserstack.user' => getenv( 'BROWSER_STACK_USERNAME' ),
+//         'browserstack.key' => getenv( 'BROWSER_STACK_ACCESS_KEY' )
+//     ]
+// ];
