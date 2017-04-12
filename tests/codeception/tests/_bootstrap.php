f384fd093aeb09eaf7946a793744c59d165b77ae
krupa <krupananda.it@gmail.com>
2017-04-12 12:25:05 +0530

    update files

--- tests/codeception/tests/_bootstrap.php
+++ tests/codeception/tests/_bootstrap.php
@@ -1,8 +1,7 @@
 <?php
 // This is global bootstrap for autoloading
-\Codeception\Configuration::$defaultSuiteSettings['modules']['config'] = [
-    'WPWebDriver' => [
-        'host' => 'hub-cloud.browserstack.com',
+\Codeception\Configuration::$defaultSuiteSettings['modules']['config']['WPWebDriver'] = [
+    'capabilities' => [
         'browserstack.user' => getenv( 'BROWSER_STACK_USERNAME' ),
         'browserstack.key' => getenv( 'BROWSER_STACK_ACCESS_KEY' )
     ]
