0d1414ea643e7bfab8ab7286abd4c2785244c96d
manish.songirkar <manish.songirkar@rtcamp.com>
2017-04-13 11:22:49 +0530

    Change Env Var for Browserstack in travis file Update acceptance.suite.yml file respective changes in travis files Remove code from bootstrap file

--- tests/codeception/tests/acceptance/_bootstrap.php
+++ tests/codeception/tests/acceptance/_bootstrap.php
@@ -1,10 +1,9 @@
 <?php
 // Here you can initialize variables that will be available to your tests
-<?php
 // This is global bootstrap for autoloading
-\Codeception\Configuration::$defaultSuiteSettings['modules']['config']['WPWebDriver'] = [
-    'capabilities' => [
-        'browserstack.user' => getenv( 'BROWSER_STACK_USERNAME' ),
-        'browserstack.key' => getenv( 'BROWSER_STACK_ACCESS_KEY' )
-    ]
-];
+// \Codeception\Configuration::$defaultSuiteSettings['modules']['config']['WPWebDriver'] = [
+//     'capabilities' => [
+//         'browserstack.user' => getenv( 'BROWSER_STACK_USERNAME' ),
+//         'browserstack.key' => getenv( 'BROWSER_STACK_ACCESS_KEY' )
+//     ]
+// ];
