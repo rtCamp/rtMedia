649232c5915f3d7986e417f52a9e619f6a8eacac
krupa <krupananda.it@gmail.com>
2017-04-19 20:09:24 +0530

    Update suite file and bootstrpa file

--- tests/codeception/tests/acceptance/_bootstrap.php
+++ tests/codeception/tests/acceptance/_bootstrap.php
@@ -1,9 +1,13 @@
 <?php
 // Here you can initialize variables that will be available to your tests
-// This is global bootstrap for autoloading
-// \Codeception\Configuration::$defaultSuiteSettings['modules']['config']['WPWebDriver'] = [
-//     'capabilities' => [
-//         'browserstack.user' => getenv( 'BROWSER_STACK_USERNAME' ),
-//         'browserstack.key' => getenv( 'BROWSER_STACK_ACCESS_KEY' )
-//     ]
-// ];
+//This is global bootstrap for autoloading
+\Codeception\Configuration::$defaultSuiteSettings['modules']['config']['WPWebDriver'] = [
+    'capabilities' => [
+        'browserstack.key' => getenv( 'BROWSERSTACK_ACCESS_KEY' ),
+        'browserstack.localIdentifier' => getenv( 'BROWSERSTACK_LOCAL_IDENTIFIER' )
+
+        // getenv('BROWSERSTACK_USERNAME') ? ($this->config["capabilities"]["browserstack.user"] = getenv('BROWSERSTACK_USERNAME')) : 0;
+        // getenv('BROWSERSTACK_ACCESS_KEY') ? ($this->config["capabilities"]["browserstack.key"] = getenv('BROWSERSTACK_ACCESS_KEY')) : 0;
+
+    ]
+];
