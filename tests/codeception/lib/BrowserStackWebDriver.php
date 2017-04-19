13b9da17de53899f34b0d4b72d02879e63cbd089
krupa <krupananda.it@gmail.com>
2017-04-19 23:18:58 +0530

    Update travis.yml file Update acceptance.suite.yml file Remove code from bootstrap file Add BrowserstackWebDriver Custom class

--- tests/codeception/lib/BrowserStackWebDriver.php
+++ tests/codeception/lib/BrowserStackWebDriver.php
@@ -1,12 +1,14 @@
 <?php
 require 'vendor/autoload.php';
 
-class BrowserStackWebDriver extends \Codeception\Module\WebDriver
+class BrowserStackWebDriver extends \Codeception\Module\WPWebDriver
 {
     private $bs_local;
     public function _initialize(){
-        getenv('BROWSERSTACK_USERNAME') ? ($this->config["capabilities"]["browserstack.user"] = getenv('BROWSERSTACK_USERNAME')) : 0;
+
+         getenv('BROWSERSTACK_USER') ? ($this->config["capabilities"]["browserstack.user"] = getenv('BROWSERSTACK_USER')) : 0;
         getenv('BROWSERSTACK_ACCESS_KEY') ? ($this->config["capabilities"]["browserstack.key"] = getenv('BROWSERSTACK_ACCESS_KEY')) : 0;
+
         if(array_key_exists("browserstack.local", $this->config["capabilities"]) && $this->config["capabilities"]["browserstack.local"])
         {
             $bs_local_args = array("key" => $this->config["capabilities"]["browserstack.key"]);
--- tests/codeception/tests/acceptance/_bootstrap.php
+++ tests/codeception/tests/acceptance/_bootstrap.php
@@ -1,13 +1,2 @@
 <?php
 // Here you can initialize variables that will be available to your tests
-//This is global bootstrap for autoloading
-\Codeception\Configuration::$defaultSuiteSettings['modules']['config']['WPWebDriver'] = [
-    'capabilities' => [
-        'browserstack.key' => getenv( 'BROWSERSTACK_ACCESS_KEY' ),
-        'browserstack.localIdentifier' => getenv( 'BROWSERSTACK_LOCAL_IDENTIFIER' )
-
-        // getenv('BROWSERSTACK_USERNAME') ? ($this->config["capabilities"]["browserstack.user"] = getenv('BROWSERSTACK_USERNAME')) : 0;
-        // getenv('BROWSERSTACK_ACCESS_KEY') ? ($this->config["capabilities"]["browserstack.key"] = getenv('BROWSERSTACK_ACCESS_KEY')) : 0;
-
-    ]
-];
