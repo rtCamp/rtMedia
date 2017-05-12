f61f6ab15bc637f942a291b998626008b8ff5200
krupa <krupananda.it@gmail.com>
2017-05-12 15:10:28 +0530

    Update selector in Constants.php file

--- tests/codeception/tests/_support/Page/Constants.php
+++ tests/codeception/tests/_support/Page/Constants.php
@@ -240,5 +240,6 @@ class Constants
     public static $manageGrpLink = '#admin';
     public static $optionsPopup = '.rtm-options';
     public static $albumMsg = ' album created successfully.';
-    public static $activitySelector = 'ul li.activity.activity_update.activity-item';
+    // public static $activitySelector = 'ul li.activity.activity_update.activity-item';
+    public static $activitySelector = '#activity-stream';
 }
