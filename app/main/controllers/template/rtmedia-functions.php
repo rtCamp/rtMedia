56c9fe6c4f55dc36d529e2d439da8fbf3cd604a9
Sidsector9 <siddharth.thevaril@gmail.com>
2017-07-17 14:34:03 +0530

    Add pagination support from attributes clicked through profile

--- app/main/controllers/template/rtmedia-functions.php
+++ app/main/controllers/template/rtmedia-functions.php
@@ -1470,9 +1470,9 @@ function rtmedia_pagination_page_link( $page_no = '' ) {
 	global $rtmedia_interaction, $rtmedia_query, $post, $rtmedia, $wp_query;
 
 	$wp_default_context = array( 'page', 'post' );
-	$rtm_context        = $wp_query->query_vars['rtm_context'];
-	$rtm_attr           = $wp_query->query_vars['rtm_attr'];
-	$rtm_term           = $wp_query->query_vars['rtm_term'];
+	$rtm_context        = get_query_var( 'rtm_context' );
+	$rtm_attr           = get_query_var( 'rtm_attr' );
+	$rtm_term           = get_query_var( 'rtm_term'  );
 
 	if ( isset( $_GET['context'] ) && in_array( $_GET['context'], $wp_default_context ) && isset( $_GET['rtmedia_shortcode'] ) && 'true' === $_GET['rtmedia_shortcode'] ) {
 		$post = get_post( intval( $_GET['context_id'] ) );
@@ -1491,7 +1491,7 @@ function rtmedia_pagination_page_link( $page_no = '' ) {
 		} else {
 			if ( $is_shortcode_on_home ) {
 				$link .= $site_url;
-			} elseif ( 'group' === $rtm_context ) {
+			} elseif ( 'group' === $rtm_context || 'profile' === $rtm_context ) {
 				$link .= $site_url . 'rtm_context/' . $rtm_context . '/attribute/' . $rtm_attr . '/' . $rtm_term . '/';
 			} else {
 				$link .= $site_url . 'author/' . $author_name . '/';
