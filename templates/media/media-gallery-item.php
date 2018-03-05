ab25504e6e03bb926c2aa2ea4a03e4b0164a409c
Mitesh Thakor <mitesh.thakor@rtcamp.com>
2018-03-05 17:33:23 +0530

    enhance code for issues 191

--- templates/media/media-gallery-item.php
+++ templates/media/media-gallery-item.php
@@ -34,8 +34,8 @@ if ( isset( $is_edit_allowed[0] ) ) {
 
 			$alt_text      = rtmedia_image_alt( false, false );
 			$rtmedia_media = '';
-                        if( $rtmedia_query ){
-                                $rtmedia_media = $rtmedia_query->rtmedia;
+                        if ( ! empty( $rtmedia_media ) && isset( $rtmedia_query->rtmedia ) ) {
+                            $rtmedia_media = $rtmedia_query->rtmedia;
                         }
 			$allowed_html  = array(
 				'span' => array(
