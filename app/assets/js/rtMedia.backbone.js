a23ec5665c1902128a3d0e107e44e7eba382b86a
Siddharth <siddharth.tikekar@rtcamp.com>
2018-03-07 18:01:34 +0530

    Issue 1059: negative-comment-count solved (with proper indentation)

--- app/assets/js/rtMedia.backbone.js
+++ app/assets/js/rtMedia.backbone.js
@@ -8,32 +8,25 @@ var rtmedia_load_template_flag = true;
 
 jQuery( function( $ ) {
 
-
 	/**
 	 * Issue 1059 fixed: negative comment count
 	 */
-	$(document).ready(function(){
-
+	$( document ).ready( function () {
 		/**
 		 * Bind dynamic event on delete button to remove media ul
 		 */
-		$('#activity-stream').on('click','.acomment-delete',function(){
-
+		$( '#activity-stream' ).on( 'click', '.acomment-delete', function () {
 			/**
 			 * get media ul
 			 */
-			let media_children = $(this).parent().parent().find('div.acomment-content ul.rtmedia-list');
-			if( media_children.length > 0 ){
-				
+			let media_children = $( this ).parent().parent().find( 'div.acomment-content ul.rtmedia-list' );
+			if ( media_children.length > 0 ) {
 				/**
 				 * remove ul if exists, so buddypress comment js doesn't get confused between media ul and child comment ul
 				 */
 				media_children.remove();
-				
 			}
-
 		});
-
 	});
 	/**
 	 * End of issue 1059 fix
