4c18d5be2d5d0e3ce72409731ad95d45840584ec
Malav Vasita <malav.vasita@rtcamp.com>
2018-03-20 14:49:33 +0530

    Update in CSS and view of media in message

--- templates/media/media-with-message.php
+++ templates/media/media-with-message.php
@@ -153,7 +153,7 @@ add_action( 'bp_after_message_content', 'show_rtm_bp_msg_media' );
 //rtm_bp_message_media_add_button function will add Media attachment button to both Compose message tab and Send a reply in BuddyPress
 function rtm_bp_message_media_add_button(){
 	?>
-	<label for="rtm_media_message_content"><?php _e( 'Attach Media ( Optional )', 'buddypress' ); ?></label>
+	<label for="rtm_media_message_content"><?php _e( 'Attach Media ( Optional )', 'buddypress-media' ); ?></label>
 	<input type="file" name="rtm_media_message_content" id="rtm_media_message_content" />
 
 	<?php
@@ -167,7 +167,13 @@ add_action( 'bp_after_message_reply_box', 'rtm_bp_message_media_add_button' );
 
 function rtm_bp_message_media_show_media(){
     ?>
-    <div class="rtm-media-for-message">
+    <div class="rtm-media-message">
+        <img src="https://static.easygenerator.com/wp-content/uploads/2013/09/demo.jpg">
+        <img src="https://static.easygenerator.com/wp-content/uploads/2013/09/demo.jpg">
+        <img src="https://static.easygenerator.com/wp-content/uploads/2013/09/demo.jpg">
+        <img src="https://static.easygenerator.com/wp-content/uploads/2013/09/demo.jpg">
+        <img src="https://static.easygenerator.com/wp-content/uploads/2013/09/demo.jpg">
+        <img src="https://static.easygenerator.com/wp-content/uploads/2013/09/demo.jpg">
         <img src="https://static.easygenerator.com/wp-content/uploads/2013/09/demo.jpg">
     </div>
 <?php
