1dc7f15f13449b9309359a8c1f28939b08db0fa5
Malav <malav.vasita@rtcamp.com>
2018-06-14 19:44:57 +0530

    Fix upload terms checkbox on activity page

--- app/assets/js/rtMedia.js
+++ app/assets/js/rtMedia.js
@@ -207,7 +207,7 @@ jQuery( 'document' ).ready( function( $ ) {
 
         // When Ajax completed attach media uploader to new activity, applay popup and attach media to comment uploader.
         jQuery(document).ajaxComplete( function(event, xhr, settings) {
-            if ( bp_template_pack && bp_template_pack === 'nouveau' ) {  
+            if ( bp_template_pack && bp_template_pack === 'nouveau' ) {
                 var get_action = get_parameter( 'action', settings.data );
                 if ( ( 'activity_filter' === get_action ||  'post_update' === get_action || 'get_single_activity_content' === get_action || 'activity_get_older_updates' === get_action ) && typeof rtmedia_masonry_layout != 'undefined' && rtmedia_masonry_layout == 'true' && typeof rtmedia_masonry_layout_activity != 'undefined' && rtmedia_masonry_layout_activity == 'true' ) {
                     setTimeout( function() {
@@ -1241,3 +1241,26 @@ function get_parameter( parameter, data ) {
 		return false;
 	}
 }
+
+/**
+ * Prevent user to upload media without checking upload terms checkbox.
+ */
+function rtm_upload_terms_activity() {
+	// Check if upload term checkbox is there.
+	if (jQuery('#rtmedia_upload_terms_conditions').length > 0) {
+		// Handle on click event.
+		jQuery('#whats-new').click('on', function () {
+			// By default prevent user to upload media.
+			jQuery('#aw-whats-new-submit').attr('disabled', 'disabled');
+			// On click of upload terms checkbox enable post button.
+			jQuery('#rtmedia_upload_terms_conditions').click(function () {
+				jQuery('#aw-whats-new-submit').attr('disabled', !this.checked)
+			});
+		});
+	}
+}
+
+jQuery(document).ready(function () {
+	// Call function when document loaded.
+	rtm_upload_terms_activity();
+});
