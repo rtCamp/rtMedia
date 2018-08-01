752492a7690e45ae28d24104451ab9eb680e79ee
ashwini.joshi@rtcamp.com <ashwini.joshi@rtcamp.com>
2018-08-01 12:32:03 +0530

    GL-255: redirect to 404 if media disabled on Profile/Group

--- app/main/routers/query/RTMediaQuery.php
+++ app/main/routers/query/RTMediaQuery.php
@@ -602,7 +602,38 @@ class RTMediaQuery {
 	}
 
 	function populate_media() {
-		global $rtmedia_query;
+		global $rtmedia_query, $rtmedia;
+
+		/**
+		 * Restrict User to access Media, if it is disabled for Profile/Group.
+		 */
+		if ( isset( $this->media_query ) && isset( $this->media_query['context'] ) && 'profile' === $this->media_query['context'] ) {
+
+			if ( isset( $rtmedia )
+			     && isset( $rtmedia->options )
+			     && isset( $rtmedia->options['buddypress_enableOnProfile'] )
+			     && 0 === intval( $rtmedia->options['buddypress_enableOnProfile'] ) ) {
+
+				global $wp_query;
+				$wp_query->set_404();
+				status_header( 404 );
+				get_template_part( 404 );
+				die;
+			}
+		} elseif ( isset( $this->media_query ) && isset( $this->media_query['context'] ) && 'group' === $this->media_query['context'] ) {
+
+			if ( isset( $rtmedia )
+			     && isset( $rtmedia->options )
+			     && isset( $rtmedia->options['buddypress_enableOnGroup'] )
+			     && 0 === intval( $rtmedia->options['buddypress_enableOnGroup'] ) ) {
+
+				global $wp_query;
+				$wp_query->set_404();
+				status_header( 404 );
+				get_template_part( 404 );
+				die;
+			}
+		}
 
 		// Check if the page is gallery shortcode or not.
 		$is_gallery_shortcode = ( isset( $rtmedia_query->is_gallery_shortcode ) && true === $rtmedia_query->is_gallery_shortcode ) ? true : false;
