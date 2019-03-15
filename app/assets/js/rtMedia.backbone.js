63a2d3b243d3ade00728b6e6a6cef9e7bf38b7f0
surajkrsingh <suraj.sk243@gmail.com>
2019-03-15 14:19:09 +0530

    Remove extra span from comment activity

--- app/assets/js/rtMedia.backbone.js
+++ app/assets/js/rtMedia.backbone.js
@@ -1689,9 +1689,7 @@ jQuery( document ).ready( function( $ ) {
 		$( this ).attr( 'disabled', 'disabled' );
 
 		// Sanitize comment content and escape html tags
-		if ( '' !== comment_content_el.val().replace(/\&nbsp;/g, '' ) ) {
-			comment_content_el.val( jQuery( '<span/>' ).text( jQuery.trim( comment_content_el.val() ) ).html() );
-		} else {
+		if ( '' === comment_content_el.val().replace(/\&nbsp;/g, '' ) ) {
 			comment_content_el.val( '' );
 		}
 
