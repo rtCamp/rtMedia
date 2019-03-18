33f905739ec477b3d6146f13e2287ea621994489
surajkrsingh <suraj.sk243@gmail.com>
2019-03-18 16:55:25 +0530

    fix formatting issue

--- app/assets/js/rtMedia.backbone.js
+++ app/assets/js/rtMedia.backbone.js
@@ -1292,7 +1292,7 @@ jQuery( document ).ready( function( $ ) {
 				 * if user want media and activity_text both require
 				 * By: Yahil
 				 */
-				if ( '' === jQuery.trim( jQuery( "#whats-new" ).val() )) {
+				if ( '' === jQuery.trim( jQuery( "#whats-new" ).val() ) ) {
 					if ( 'disable' !== rtmedia_activity_text_with_attachment ) {
 						jQuery('#whats-new-form').prepend('<div id="message" class="error bp-ajax-message" style="display: block;"><p> ' + rtmedia_empty_activity_msg + ' </p></div>')
 						jQuery( '#whats-new' ).removeAttr( 'disabled' );
