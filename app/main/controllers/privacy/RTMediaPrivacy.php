ee6e52d5cd668c6cc9d1784714bb66271d468726
Vaishali Agola <vaishali.agola@rtcamp.com>
2020-01-23 17:34:43 +0530

    Change condition for default privacy checking

--- app/main/controllers/privacy/RTMediaPrivacy.php
+++ app/main/controllers/privacy/RTMediaPrivacy.php
@@ -466,7 +466,7 @@ class RTMediaPrivacy {
 		$default_privacy = sanitize_text_field( filter_input( INPUT_POST, 'rtmedia-default-privacy', FILTER_SANITIZE_STRING ) );
 		$nonce           = sanitize_text_field( filter_input( INPUT_POST, 'rtmedia_member_settings_privacy', FILTER_SANITIZE_STRING ) );
 
-		if ( ! empty( $default_privacy ) || '0' === $default_privacy ) {
+		if ( ! empty( $default_privacy ) || 0 === intval( $default_privacy ) ) {
 
 			$status = false;
 			if ( wp_verify_nonce( $nonce, 'rtmedia_member_settings_privacy' ) ) {
