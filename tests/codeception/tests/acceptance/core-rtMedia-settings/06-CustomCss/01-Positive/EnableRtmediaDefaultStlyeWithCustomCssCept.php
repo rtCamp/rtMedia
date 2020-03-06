e43e4c2968ac3e2b10b01bb136fbdf317ab4c28a
kjnanda <krupananda.it@gmail.com>
2020-03-06 17:58:40 +0530

    Add changes in EnableRtmediaDefaultStlyeWithCustomCssCept.php file to check the failure of this testcase with travis build

--- tests/codeception/tests/acceptance/core-rtMedia-settings/06-CustomCss/01-Positive/EnableRtmediaDefaultStlyeWithCustomCssCept.php
+++ tests/codeception/tests/acceptance/core-rtMedia-settings/06-CustomCss/01-Positive/EnableRtmediaDefaultStlyeWithCustomCssCept.php
@@ -30,6 +30,8 @@
     $settings->setValue( ConstantsPage::$customCssLabel, ConstantsPage::$cssTextarea, ConstantsPage::$customCssValue );
     $settings->saveSettings();
 
+    $I->reloadPage();
+
     $buddypress = new BuddypressSettingsPage( $I );
     $buddypress->gotoActivity();
 
@@ -37,6 +39,17 @@
     return $webdriver->findElement(WebDriverBy::cssSelector('textarea#whats-new'))->getCSSValue('border-color');
     });
 
-    $I->assertEquals( $bar, 'rgb(255, 0, 0)' );
+    $I->wait(5);
+
+    $I->fillField('textarea#whats-new', 'HI..!I see this red color border!');
+    $I->waitForElementVisible( ConstantsPage::$postUpdateButton, 60);
+
+    $expected = 'rgb(255, 0, 0)';
+    $actual = $bar;
+
+    echo "Exepcted textarea border color = " . $expected . "\n";
+    echo "Actual textarea border color = " . $actual . "\n";
+    // $I->assertEquals( $bar, 'rgb(255, 0, 0)' );
+    $I->assertEquals( $actual, $expected );
 
 ?>
