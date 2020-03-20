deda60e58624129319d895b52bcc662d33cd32f9
Adarsh Verma <adarsh.srmcem@gmail.com>
2020-03-20 16:20:04 +0530

    Used 'strval' to match the privacy levels (integer) to the saved privacy value (string).

--- app/admin/RTMediaFormHandler.php
+++ app/admin/RTMediaFormHandler.php
@@ -170,7 +170,7 @@ class RTMediaFormHandler {
 		foreach ( $args['radios'] as $value => $key ) {
 			$args['rtForm_options'][] = array(
 				$key      => $value,
-				'checked' => ( $args['default'] == $value ) ? true : false, // phpcs:ignore WordPress.PHP.StrictComparisons.LooseComparison -- The == error ignored as the privacy levels indexes are integer, and while saving in database, the saved value becomes string.
+				'checked' => ( strval( $args['default'] ) === strval( $value ) ) ? true : false, // strval is used to fix the privacy levels being integer to match the saved privacy value.
 			);
 		}
 
