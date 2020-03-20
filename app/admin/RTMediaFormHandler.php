57ea353d8e4ecb87fbaf52462877d6740b41b47b
Adarsh Verma <adarsh.srmcem@gmail.com>
2020-03-20 15:30:53 +0530

    Fixed the error of radio buttons, not working after saving settings.

--- app/admin/RTMediaFormHandler.php
+++ app/admin/RTMediaFormHandler.php
@@ -170,7 +170,7 @@ class RTMediaFormHandler {
 		foreach ( $args['radios'] as $value => $key ) {
 			$args['rtForm_options'][] = array(
 				$key      => $value,
-				'checked' => ( (int) $args['default'] === $value ) ? true : false,
+				'checked' => ( $args['default'] == $value ) ? true : false, // phpcs:ignore WordPress.PHP.StrictComparisons.LooseComparison The == error ignored as the privacy levels indexes are integer, and while saving in database, the saved value becomes string.
 			);
 		}
 
