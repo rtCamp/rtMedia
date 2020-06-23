b0c3b29912b4439ee295db0a097211d0272704bc
Siddharth Tikekar <siddharth.tikekar@rtcamp.com>
2020-06-23 15:05:51 +0530

    Fix indentation, Yoda condition and strict type checking

--- app/assets/js/rtMedia.backbone.js
+++ app/assets/js/rtMedia.backbone.js
@@ -717,11 +717,11 @@ jQuery( function( $ ) {
 						    var file_desc_val = jQuery( rtm_file_desc_input ).val();
 						    var file_name_wrapper_el = jQuery( rtm_file_label ).siblings( '.plupload_file_name_wrapper' );
 
-						    if ( file_title_val != '' ) {
+							if ( '' !== trim( file_title_val ) ) {
 								var extension = file.name.split( '.' )[1];
 								file_name_wrapper_el.text( file_title_val + '.' + extension );
 								file.title = file_title_val;
-						    }
+							}
 
 						    if ( file_desc_val != '' ) {
 						        file.description = file_desc_val;
@@ -1286,7 +1286,7 @@ jQuery( document ).ready( function( $ ) {
 
 					var file_name_wrapper_el = jQuery( rtm_file_label ).siblings( '.plupload_file_name_wrapper' );
 
-					if ( file_title_val != '' ) {
+					if ( '' !== trim( file_title_val ) ) {
 						var extension = file.name.split( '.' )[1];
 						file_name_wrapper_el.text( file_title_val + '.' + extension );
 						file.title = file_title_val;
@@ -2633,7 +2633,7 @@ function renderUploadercomment_media( widget_id, parent_id_type ) {
 
 					var file_name_wrapper_el = jQuery( rtm_file_label ).siblings( '.plupload_file_name_wrapper' );
 
-					if ( file_title_val != '' ) {
+					if ( '' !== trim( file_title_val ) ) {
 						var extension = file.name.split( '.' )[1];
 						file_name_wrapper_el.text( file_title_val + '.' + extension );
 						file.title = file_title_val;
