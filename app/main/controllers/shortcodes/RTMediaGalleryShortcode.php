4ce6b616b38ccaad39cacbbba2968c80eaaef21a
Vipin Kumar Dinkar <vipinkumard365@gmail.com>
2021-01-08 17:34:59 +0530

    update wp_localize_Script in shortcode gallery

--- app/main/controllers/shortcodes/RTMediaGalleryShortcode.php
+++ app/main/controllers/shortcodes/RTMediaGalleryShortcode.php
@@ -245,7 +245,7 @@ class RTMediaGalleryShortcode {
 
 				$template_url = esc_url( add_query_arg( $media_template_args, admin_url( 'admin-ajax.php' ) ), null, '' );
 			}
-			wp_localize_script( 'rtmedia-backbone', 'template_url', $template_url );
+			wp_localize_script( 'rtmedia-backbone', 'template_url', array( 'data' => $template_url ) );
 
 			/**
 			 * Remove search_filter parameter from attr,
