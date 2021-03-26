98f1488d1c3d10f800571ca22402668a297b40fc
Tremi Dkhar <tremi@dkhar.com>
2021-03-26 18:28:07 +0530

    Fixed wp_localize_script() not using the correct handle

--- app/main/controllers/shortcodes/RTMediaGalleryShortcode.php
+++ app/main/controllers/shortcodes/RTMediaGalleryShortcode.php
@@ -94,7 +94,7 @@ class RTMediaGalleryShortcode {
 
 			$template_url = esc_url( add_query_arg( $media_template_args, admin_url( 'admin-ajax.php' ) ), null, '' );
 		}
-		wp_localize_script( 'rtmedia-backbon', 'rtmedia_template', array( 'url' => $template_url ) );
+		wp_localize_script( 'rtmedia-backbone', 'rtmedia_template', array( 'url' => $template_url ) );
 
 		$request_uri = rtm_get_server_var( 'REQUEST_URI', 'FILTER_SANITIZE_URL' );
 		$url         = rtmedia_get_upload_url( $request_uri );
@@ -247,7 +247,7 @@ class RTMediaGalleryShortcode {
 				$template_url = esc_url( add_query_arg( $media_template_args, admin_url( 'admin-ajax.php' ) ), null, '' );
 			}
 			// wp_localize_script( 'rtmedia-backbone', 'template_url', $template_url );
-			wp_localize_script( 'rtmedia-backbon', 'rtmedia_template', array( 'url' => $template_url ) );
+			wp_localize_script( 'rtmedia-backbone', 'rtmedia_template', array( 'url' => $template_url ) );
 
 			/**
 			 * Remove search_filter parameter from attr,
