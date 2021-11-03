f28a2ee5f4718d7df264c0067964419f0b99da59
Pradeep Pal <pradeeppal156@gmail.com>
2021-11-04 01:26:42 +0530

    fix loadmore ui

--- templates/media/media-gallery.php
+++ templates/media/media-gallery.php
@@ -113,9 +113,7 @@ $rand_id = wp_rand( 0, 1000 );
 				rtmedia_media_pagination();
 			} else {
 				$display = '';
-				if ( rtmedia_offset() + rtmedia_per_page_media() < rtmedia_count() ) {
-					$display = 'display:block;';
-				} else {
+				if ( rtmedia_offset() + rtmedia_per_page_media() > rtmedia_count() ) {
 					$display = 'display:none;';
 				}
 				?>
