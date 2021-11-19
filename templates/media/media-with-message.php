ac23adbf433197022be8f150c5822ef5cffad1c0
Pradeep Pal <pradeeppal156@gmail.com>
2021-11-19 22:55:23 +0530

    remove demo image

--- templates/media/media-with-message.php
+++ templates/media/media-with-message.php
@@ -11,7 +11,7 @@
  *  This function will place uploader in BuddyPress compose message form and handle send reply activity.
  */
 function rtm_bp_message_media_add_upload_media_button() {
-?>
+	?>
 	<script>
 		$(function(){
 			$("#send-notice").click(function () {
@@ -22,39 +22,39 @@ function rtm_bp_message_media_add_upload_media_button() {
 				}
 			} );
 		} );
-        jQuery( document ).ready( function() {
-            handler = function() {
-                var order = jQuery( '#messages_order' ).val() || 'ASC',
-                    offset = jQuery( '#message-recipients' ).offset();
-                var button = jQuery( 'input#send_reply_button' );
-                jQuery(button).addClass( 'loading' );
-                jQuery.post( ajaxurl, {
-                        action                  : 'messages_send_reply',
-                        'cookie'                : bp_get_cookies(),
-                        '_wpnonce'              : jQuery( 'input#send_message_nonce' ).val(),
-                        'content'               : jQuery( '#message_content' ).val(),
-                        'send_to'               : jQuery( 'input#send_to' ).val(),
-                        'subject'               : jQuery( 'input#subject' ).val(),
-                        'thread_id'             : jQuery( 'input#thread_id' ).val(),
-                        'rtm_bpm_uploaded_media': jQuery( 'input#rtm_bpm_uploaded_media' ).val()
-                    },
-                    function( response )
-                    {
-                        if ( response[0] + response[1] == "-1" ) {
-                            jQuery('form#send-reply').prepend( response.substr( 2, response.length ) );
-                        } else {
-                            jQuery('form#send-reply div#message').remove();
-                            jQuery("#message_content").val('');
-                            jQuery("#rtm_bpm_uploaded_media").removeAttr('value');
-                            if ( 'ASC' == order ) {
-                                jQuery('form#send-reply').before( response );
-                            } else {
-                                jQuery('#message-recipients').after( response );
-                                jQuery(window).scrollTop(offset.top);
-                            }
-                        jQuery( ".new-message" ).hide().slideDown( 200, function() {
-                            jQuery( '.new-message' ).removeClass('new-message');
-                        } );
+		jQuery( document ).ready( function() {
+			handler = function() {
+				var order = jQuery( '#messages_order' ).val() || 'ASC',
+					offset = jQuery( '#message-recipients' ).offset();
+				var button = jQuery( 'input#send_reply_button' );
+				jQuery(button).addClass( 'loading' );
+				jQuery.post( ajaxurl, {
+						action                  : 'messages_send_reply',
+						'cookie'                : bp_get_cookies(),
+						'_wpnonce'              : jQuery( 'input#send_message_nonce' ).val(),
+						'content'               : jQuery( '#message_content' ).val(),
+						'send_to'               : jQuery( 'input#send_to' ).val(),
+						'subject'               : jQuery( 'input#subject' ).val(),
+						'thread_id'             : jQuery( 'input#thread_id' ).val(),
+						'rtm_bpm_uploaded_media': jQuery( 'input#rtm_bpm_uploaded_media' ).val()
+					},
+					function( response )
+					{
+						if ( response[0] + response[1] == "-1" ) {
+							jQuery('form#send-reply').prepend( response.substr( 2, response.length ) );
+						} else {
+							jQuery('form#send-reply div#message').remove();
+							jQuery("#message_content").val('');
+							jQuery("#rtm_bpm_uploaded_media").removeAttr('value');
+							if ( 'ASC' == order ) {
+								jQuery('form#send-reply').before( response );
+							} else {
+								jQuery('#message-recipients').after( response );
+								jQuery(window).scrollTop(offset.top);
+							}
+						jQuery( ".new-message" ).hide().slideDown( 200, function() {
+							jQuery( '.new-message' ).removeClass('new-message');
+						} );
 					}
 					jQuery(button).removeClass('loading');
 				});
@@ -73,25 +73,25 @@ function rtm_bp_message_media_add_upload_media_button() {
 	<div id="rtm-media-gallery-uploader" class="rtm-media-gallery-uploader">
 		<?php
 		rtmedia_uploader(
-			[
+			array(
 				'is_up_shortcode' => false,
 				'allow_anonymous' => true,
 				'privacy_enabled' => false,
 				'context'         => 'message-media',
 				'context_id'      => get_current_user_id(),
-			]
+			)
 		);
-			?>
+		?>
 	</div>
 	<input type="hidden" id="rtm_bpm_uploaded_media" name="rtm_bpm_uploaded_media" />
 	<?php
 }
 
 /**
-* Add additional parameter media ID in BuddyPress message SEND MESSAGE process. and Insert data into MEDIA_META table.
-*
-* @param object $message Getting parameter response when sending message.
-**/
+ * Add additional parameter media ID in BuddyPress message SEND MESSAGE process. and Insert data into MEDIA_META table.
+ *
+ * @param object $message Getting parameter response when sending message.
+ **/
 function rtm_add_message_media_params( $message ) {
 	$insert_media_object  = new RTDBModel( 'rtm_media_meta' );
 	$message->media_array = filter_input( INPUT_POST, 'rtm_bpm_uploaded_media' );
@@ -99,13 +99,13 @@ function rtm_add_message_media_params( $message ) {
 	if ( ! empty( $media ) && null !== $media ) {
 		foreach ( $media as $media_id ) {
 			$insert_media_object->insert(
-				[
+				array(
 					'media_id'   => $media_id,
-					// Adding meta data into custom meta table
-					'meta_key'   => 'rtm-bp-message-media', // phpcs:ignore
-					// Adding meta data into custom meta table
+					// Adding meta data into custom meta table.
+					'meta_key'   => 'rtm-bp-message-media',
+					// Adding meta data into custom meta table.
 					'meta_value' => $message->id, // phpcs:ignore
-				]
+				)
 			);
 		}
 	}
@@ -147,13 +147,13 @@ function show_rtm_bp_msg_media() {
 	$rtm_change_alt_text_filter = apply_filters( 'rtmc_change_alt_text', $alt_text, $rtmedia_media );
 
 	if ( '0' !== $media_result[0]->media_id ) {
-	?>
-	<ul class="rtmedia-list-media rtm-gallery-list clearfix" style = "margin-top: 10px;">
-	<?php
-	foreach ( $media_result as $media_result_array_value ) {
-		$media     = rtmedia_image( 'rt_media_thumbnail', $media_result_array_value->media_id, false );
-		$media_url = $url[0] . '/media/' . $media_result_array_value->media_id . '/';
 		?>
+	<ul class="rtmedia-list-media rtm-gallery-list clearfix" style = "margin-top: 10px;">
+		<?php
+		foreach ( $media_result as $media_result_array_value ) {
+			$media     = rtmedia_image( 'rt_media_thumbnail', $media_result_array_value->media_id, false );
+			$media_url = $url[0] . '/media/' . $media_result_array_value->media_id . '/';
+			?>
 
 			<li class="rtmedia-list-item" style="display:inline; float: left;" id="<?php echo $media_result_array_value->media_id; // @codingStandardsIgnoreLine?>">
 				<a href="<?php echo esc_attr( $media_url ); ?>" class="<?php echo esc_attr( apply_filters( 'rtmedia_gallery_list_item_a_class', 'rtmedia-list-item-a' ) ); ?>">
@@ -165,7 +165,7 @@ function show_rtm_bp_msg_media() {
 
 		<?php } ?>
 	</ul>
-	<?php
+		<?php
 	}
 }
 
@@ -179,33 +179,22 @@ add_action( 'messages_message_sent', 'rtm_add_message_media_params' );
 add_action( 'bp_after_message_content', 'show_rtm_bp_msg_media' );
 
 
-//rtm_bp_message_media_add_button function will add Media attachment button to both Compose message tab and Send a reply in BuddyPress
-function rtm_bp_message_media_add_button(){
+/**
+ * Rtm_bp_message_media_add_button function will add Media attachment button to both Compose message tab and Send a reply in BuddyPress.
+ *
+ * @return void
+ */
+function rtm_bp_message_media_add_button() {
 	?>
-	<label for="rtm_media_message_content"><?php _e( 'Attach Media ( Optional )', 'buddypress-media' ); ?></label>
+	<label for="rtm_media_message_content"><?php esc_attr_e( 'Attach Media ( Optional )', 'buddypress-media' ); ?></label>
 	<input type="file" name="rtm_media_message_content" id="rtm_media_message_content" />
 
 	<?php
 }
 
-//Adding Browse button under message in Compose tab
+// Adding Browse button under message in Compose tab.
 add_action( 'bp_after_messages_compose_content', 'rtm_bp_message_media_add_button' );
 
-//Adding Browse button under message in Send reply tab
+// Adding Browse button under message in Send reply tab.
 add_action( 'bp_after_message_reply_box', 'rtm_bp_message_media_add_button' );
 
-function rtm_bp_message_media_show_media(){
-    ?>
-    <div class="rtm-media-message">
-        <img src="https://static.easygenerator.com/wp-content/uploads/2013/09/demo.jpg">
-        <img src="https://static.easygenerator.com/wp-content/uploads/2013/09/demo.jpg">
-        <img src="https://static.easygenerator.com/wp-content/uploads/2013/09/demo.jpg">
-        <img src="https://static.easygenerator.com/wp-content/uploads/2013/09/demo.jpg">
-        <img src="https://static.easygenerator.com/wp-content/uploads/2013/09/demo.jpg">
-        <img src="https://static.easygenerator.com/wp-content/uploads/2013/09/demo.jpg">
-        <img src="https://static.easygenerator.com/wp-content/uploads/2013/09/demo.jpg">
-    </div>
-<?php
-}
-
-add_action( 'bp_after_message_content', 'rtm_bp_message_media_show_media' );
