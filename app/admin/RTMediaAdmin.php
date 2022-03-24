d4637fa917dc7e367f24e517676c35c5ac96ea73
Pradeep Pal <pradeeppal156@gmail.com>
2022-03-24 17:01:00 +0530

    fix minor ui change

--- app/admin/RTMediaAdmin.php
+++ app/admin/RTMediaAdmin.php
@@ -501,10 +501,12 @@ if ( ! class_exists( 'RTMediaAdmin' ) ) {
 				}
 				rtmedia_update_site_option( 'rtmedia-addon-update-notice-3_8', 'show' );
 				?>
-				<div class="notice is-dismissible rtmedia-addon-update-notice">
+				<div class="notice error is-dismissible rtmedia-addon-update-notice">
 					<p>
 					<?php
-						$message = apply_filters( 'rt_addon_update_notice', sprintf( __( ' <strong> rtMedia: </strong> rtMedia Premium update is available. Please update it from the plugins or download it from <a href = "https://rtmedia.io/my-account/" target="_blank" >your account</a>', 'buddypress-media' ) ) );
+						$message = apply_filters( 'rt_addon_update_notice', sprintf( __( ' rtMedia Premium update is available. Please update it from the plugins or download it from <a href = "https://rtmedia.io/my-account/" target="_blank" >your account</a>', 'buddypress-media' ) ) ); ?>
+						<b><?php esc_html_e( 'rtMedia: ', 'buddypress-media' ); ?></b>
+						<?php
 						echo wp_kses(
 							$message,
 							array(
@@ -512,7 +514,6 @@ if ( ! class_exists( 'RTMediaAdmin' ) ) {
 									'href' => array(),
 									'target' => array(),
 								),
-								'strong' => array(),
 							)
 						);
 						?>
