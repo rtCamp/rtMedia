1cfeaef21a530f61cdb1b6b58989f303abdb72bd
Mukul Singh <mukulsingh3344@gmail.com>
2022-03-30 12:56:52 +0530

    Fix the subscribe button design

--- app/admin/RTMediaAdmin.php
+++ app/admin/RTMediaAdmin.php
@@ -504,7 +504,8 @@ if ( ! class_exists( 'RTMediaAdmin' ) ) {
 				<div class="notice error is-dismissible rtmedia-addon-update-notice">
 					<p>
 					<?php
-						$message = apply_filters( 'rt_addon_update_notice', sprintf( __( ' rtMedia Premium update is available. Please update it from the plugins or download it from <a href = "https://rtmedia.io/my-account/" target="_blank" >your account</a>', 'buddypress-media' ) ) ); ?>
+						$message = apply_filters( 'rt_addon_update_notice', sprintf( __( ' rtMedia Premium update is available. Please update it from the plugins or download it from <a href = "https://rtmedia.io/my-account/" target="_blank" >your account</a>', 'buddypress-media' ) ) );
+					?>
 						<b><?php esc_html_e( 'rtMedia: ', 'buddypress-media' ); ?></b>
 						<?php
 						echo wp_kses(
