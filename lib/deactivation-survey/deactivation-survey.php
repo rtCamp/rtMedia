c029608feb63f869e041c6586bb367610eaaef61
robicse11127 <robizmailbox@gmail.com>
2022-07-29 16:33:29 +0600

    Added deactivation popup module

--- lib/deactivation-survey/deactivation-survey.php
+++ lib/deactivation-survey/deactivation-survey.php
@@ -1,71 +1,132 @@
 <?php
 /**
- * Loading scripts
- *
- * @return void
+ * Plguin Deactivation Survey Class.
  */
-function load_scripts() {
-    wp_register_style( 'rt-deactivation-survey', RTMEDIA_URL . '/lib/deactivation-survey/deactivation-survey.css', [], wp_rand(), 'all' );
-    wp_register_script( 'rt-deactivation-survey', RTMEDIA_URL . '/lib/deactivation-survey/deactivation-survey.js', [ 'jquery' ], wp_rand(), true );
+class Deactivation_Survey {
 
-    wp_enqueue_style( 'rt-deactivation-survey' );
-    wp_enqueue_script( 'rt-deactivation-survey' );
+    /**
+     * API Url.
+     *
+     * @var string
+     */
+    public $api_url = 'https://rtmedia-dev.rtm.rt.gw/wp-json/rtps/v1/survey'; // Replace the API Url by the production API Url.
 
-    $reasons = [
-        'I could\'t understand how to make it work.',
-        'I found a better plugin.',
-        'The plugin is greate but I need spicific features, that you do\'t support.',
-        'The plugin is\'t working.',
-        'It\'s not what I was looking for.',
-        'The plugin didn\'t work as expected.',
-        'Others.'
-    ];
+    /**
+     * Basic Auth Username
+     *
+     * @var string
+     */
+    private $auth_user = 'rabiul'; // Replace the admin user by the production admin user.
 
-    $current_user = wp_get_current_user();
+    /**
+     * Basic Auth Password.
+     *
+     * @var string
+     */
+    private $auth_password = 'SxQf 1BT0 bYcZ YU3d cQkS aT3Z'; // Replcae the auth token by the production auth token.
 
-    $rt_deactivate = [
-        'home_url'    => home_url(),
-        'admin_url'   => admin_url(),
-        'ajax_url'    => admin_url( 'admin-ajax.php' ),
-        'nonce'       => wp_create_nonce( 'rtmedia' ),
-        'reasons'     => wp_json_encode( $reasons ),
-        'user_name'   => $current_user->user_nicename,
-        'user_email'  => $current_user->user_email,
-        'header_text' => esc_html__( 'If you have a moment, please let us know why you are deactivating: ' )
-    ];
-
-    wp_localize_script( 'rt-deactivation-survey', 'rtDeactivate', $rt_deactivate );
-}
-add_action( 'admin_enqueue_scripts', 'load_scripts' );
-
-/**
- * Ajax Function call
- */
-function rt_send_deactivation_feedback() {
-    // Checking ajax referer.
-    check_ajax_referer( 'rtmedia', 'nonce' );
-
-    if ( ! $_POST['reason'] && empty( $_POST['user'] && ! $_POST['site_url'] ) ) {
-        return;
+    /**
+     * Constructor function.
+     */
+    public function __construct() {
+        add_action( 'admin_enqueue_scripts', [ $this, 'load_scripts' ] );
+        add_action( 'wp_ajax_rt_send_deactivation_feedback', [ $this, 'rt_send_deactivation_feedback' ] );
     }
 
-    // Filter the inputs.
-    $site_url = filter_input( INPUT_POST, 'site_url', FILTER_SANITIZE_URL );
-    $reason   = filter_input( INPUT_POST, 'reason', FILTER_SANITIZE_STRING );
-    $user     = filter_input( INPUT_POST, 'user', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY );
+    /**
+     * Loading scripts.
+     *
+     * @return void
+     */
+    public function load_scripts() {
+        global $pagenow;
 
-    $data = [
-        'reason'   => $reason,
-        'username' => $user['name'],
-        'email'    => $user['email'],
-        'site_url' => $site_url
-    ];
+        wp_register_style( 'rt-deactivation-survey', RTMEDIA_URL . '/lib/deactivation-survey/deactivation-survey.css', [], wp_rand(), 'all' );
+        wp_register_script( 'rt-deactivation-survey', RTMEDIA_URL . '/lib/deactivation-survey/deactivation-survey.js', [ 'jquery' ], wp_rand(), true );
 
-    // echo '<pre>';
-    // print_r( $data );
-    // echo '</pre>';die(  );
+        if ( is_admin() && 'plugins.php' === $pagenow ) {
 
-    echo wp_json_encode( 'success' );
-    wp_die();
+            wp_enqueue_style( 'rt-deactivation-survey' );
+            wp_enqueue_script( 'rt-deactivation-survey' );
+    
+            $reasons = [
+                'I could\'t understand how to make it work.',
+                'I found a better plugin.',
+                'The plugin is greate but I need spicific features, that you do\'t support.',
+                'The plugin is\'t working.',
+                'It\'s not what I was looking for.',
+                'The plugin didn\'t work as expected.',
+            ];
+    
+            $current_user = wp_get_current_user();
+    
+            $rt_deactivate = [
+                'home_url'    => home_url(),
+                'admin_url'   => admin_url(),
+                'ajax_url'    => admin_url( 'admin-ajax.php' ),
+                'nonce'       => wp_create_nonce( 'rtmedia' ),
+                'reasons'     => wp_json_encode( $reasons ),
+                'user_name'   => $current_user->user_nicename,
+                'user_email'  => $current_user->user_email,
+                'header_text' => esc_html__( 'If you have a moment, please let us know why you are deactivating: ', 'buddypress-media' )
+            ];
+    
+            wp_localize_script( 'rt-deactivation-survey', 'rtDeactivate', $rt_deactivate );
+        }
+    }
+
+    /**
+     * Ajax Function call.
+     *
+     * @return string.
+     */
+    public function rt_send_deactivation_feedback() {
+        // Checking ajax referer.
+        check_ajax_referer( 'rtmedia', 'nonce' );
+
+        if ( ! $_POST['reason'] && empty( $_POST['user'] && ! $_POST['site_url'] ) ) {
+            return;
+        }
+
+        // Filter the inputs.
+        $site_url = filter_input( INPUT_POST, 'site_url', FILTER_SANITIZE_URL );
+        $reason   = filter_input( INPUT_POST, 'reason', FILTER_SANITIZE_STRING );
+        $user     = filter_input( INPUT_POST, 'user', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY );
+
+        $data = [
+            'plugin_name' => 'rtMedia Core',
+            'plugin_slug' => 'rtmedia-core',
+            'site_url'    => $site_url,
+            'reason'      => $reason,
+            'user_name'   => $user['name'],
+            'user_email'  => $user['email'],
+        ];
+
+        $auth_user     = $this->auth_user;
+        $auth_password = $this->auth_password;
+
+        $options = [
+            'body'             => $data,
+            'headers'          => [
+                'Content-type' => "application/x-www-form-urlencoded",
+                'Authorization' => "Basic " . base64_encode("${auth_user}:${auth_password}")
+            ],
+            'timeout'          => 60,
+            'redirection'      => 5,
+            'httpversion'      => '1.0',
+            'sslverify'        => false,
+            'data_format'      => 'body'
+        ];
+
+        $api_response = wp_remote_post( $this->api_url, $options );
+        $response     = json_decode( wp_remote_retrieve_body( $api_response ) );
+
+        if ( 'integer' === gettype( $response ) ) {
+            echo wp_json_encode( 'success' );
+        }
+        wp_die();
+    }
+
 }
-add_action( 'wp_ajax_rt_send_deactivation_feedback', 'rt_send_deactivation_feedback' );
+
+new Deactivation_Survey();
