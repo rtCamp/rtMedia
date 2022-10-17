a312dd7a272ce51196fbf66cbf2c431c1c83d619
Pradeep Pal <pradeeppal156@gmail.com>
2022-10-17 23:23:06 +0530

    remove function

--- templates/upload/uploader.php
+++ templates/upload/uploader.php
@@ -163,19 +163,3 @@ if ( is_array( $tabs ) && count( $tabs ) ) { ?>
 	<?php
 }
 
-/**
- * Display hidden fields.
- *
- * @param string $key   Input key.
- * @param string $value Input value.
- *
- * @since 4.6.11
- *
- * @return void
- */
-function rtmedia_uploader_hidden_fields( $key, $value ) {
-
-	printf( '<input type="hidden" name="%1$s" value="%2$s" />', esc_attr( $key ), esc_attr( $value ) );
-}
-
-
