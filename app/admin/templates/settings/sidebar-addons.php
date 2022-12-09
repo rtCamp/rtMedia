34f71ac709c20b7d5c137ab2ad84604049f10e61
Pradeep Pal <pradeeppal156@gmail.com>
2022-12-09 12:10:41 +0530

    fix the escaping issue from all icons

--- app/admin/templates/settings/sidebar-addons.php
+++ app/admin/templates/settings/sidebar-addons.php
@@ -8,36 +8,24 @@
 ?>
 
 <div id="social" class="rtm-social-share">
-	<p>
-		<a href="http://twitter.com/home/?status=<?php echo esc_attr( $rtmedia_sidebar_message ); ?>"
-			class="button twitter" target= "_blank" title="<?php esc_attr_e( 'Post to Twitter Now', 'buddypress-media' ); ?>"
-		>
-			<?php esc_html_e( 'Post to Twitter', 'buddypress-media' ); ?>
-			<span class="dashicons dashicons-twitter"></span>
-		</a>
-	</p>
-	<p>
-		<a href="https://www.facebook.com/sharer/sharer.php?u=https://rtmedia.io/" class="button facebook" target="_blank"
-			title="<?php esc_attr_e( 'Share on Facebook Now', 'buddypress-media' ); ?>"
-		>
-			<?php esc_html_e( 'Share on Facebook', 'buddypress-media' ); ?>
-			<span class="dashicons dashicons-facebook"></span>
-		</a>
-	</p>
-	<p>
-		<a href="https://wordpress.org/support/plugin/buddypress-media/reviews/#new-post" class="button wordpress" target= "_blank"
-			title="<?php esc_attr_e( 'Rate rtMedia on Wordpress.org', 'buddypress-media' ); ?>"
-		>
-			<?php esc_html_e( 'Rate on Wordpress.org', 'buddypress-media' ); ?>
-			<span class="dashicons dashicons-wordpress"></span>
-		</a>
-	</p>
-	<p>
-		<a href="https://rtmedia.io/feed/" class="button rss" target="_blank"
-		   title="<?php esc_attr_e( 'Subscribe to our Feeds', 'buddypress-media' ); ?>"
-		>
-			<?php esc_html_e( 'Subscribe to our Feeds', 'buddypress-media' ); ?>
-			<span class="dashicons dashicons-rss"></span>
-		</a>
-	</p>
+	<a href="http://twitter.com/home/?status=" class="button twitter" target= "_blank" title="' . <?php echo esc_attr__( 'Post to Twitter Now', 'buddypress-media' ); ?> . '">
+		<span class="dashicons dashicons-twitter"></span>
+			<span class="icon-message"> <?php echo esc_html__( 'Post to Twitter', 'buddypress-media' ); ?> </span>
+	</a>
+
+	<a href="https://www.facebook.com/sharer/sharer.php?u=https://rtmedia.io/" class="button facebook" target="_blank" title="' . <?php echo esc_attr__( 'Share on Facebook Now', 'buddypress-media' ); ?> . '">
+		<span class="dashicons dashicons-facebook"></span>
+			<span class="icon-message"> <?php echo esc_html__( 'Post to Facebook', 'buddypress-media' ); ?></span>
+	</a>
+
+	<a href="https://wordpress.org/support/plugin/buddypress-media/reviews/#new-post" class="button wordpress" target= "_blank" title="' . <?php echo esc_attr__( 'Rate rtMedia on Wordpress.org', 'buddypress-media' ); ?> . '">
+		<span class="dashicons dashicons-wordpress"></span>
+			<span class="icon-message"> <?php echo esc_html__( 'Rate us on Wordpress.org', 'buddypress-media' ); ?> </span>			
+	</a>
+
+	<a href="' . sprintf( '%s', 'https://rtmedia.io/feed/' ) . '" class="button rss" target="_blank" title="' . <?php echo esc_attr__( 'Subscribe to our Feeds', 'buddypress-media' ); ?> . '">
+		<span class="dashicons dashicons-rss"></span>
+			<span class="icon-message"> <?php echo esc_html__( 'Subscribe to our Feeds', 'buddypress-media' ); ?> </span>
+	</a>
+
 </div>
