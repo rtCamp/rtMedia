a0e60513605fd5dac417668278b9d2aa4b626689
Utsav-Ladani <201901076@daiict.ac.in>
2023-12-29 11:32:01 +0530

    Bind correct context for Nouveau inject method

--- app/assets/js/rtMedia.activity.js
+++ app/assets/js/rtMedia.activity.js
@@ -4,7 +4,7 @@
 
         // Overwrite the inject function and apply mediaelement library player after adding activity.
         bp.Nouveau.inject = function( selector, content, method ) {
-            callback( selector, content, method );
+            callback.bind(this)( selector, content, method );
 
             if ( 'function' === typeof rtmedia_on_activity_add ) {
                 rtmedia_on_activity_add();
