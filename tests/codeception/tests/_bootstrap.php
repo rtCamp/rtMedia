5e8019663293f57f38e07a951cd67dbb14b3bec8
Indira Biswas <indirabiswas@Indiras-MacBook-Air.local>
2026-03-04 16:42:54 +0530

    Remove tests directory to fix Dependabot error

--- tests/codeception/tests/_bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// This is global bootstrap for autoloading
-// \Codeception\Configuration::$defaultSuiteSettings['modules']['config']['WPWebDriver'] = [
-//     'capabilities' => [
-//         'browserstack.user' => getenv('BROWSERSTACK_USER'),
-//         'browserstack.key' => getenv('BROWSERSTACK_ACCESS_KEY')
-//     ]
-// ];
